475b0859e3f005a3761d4c3463e13712f2cdbad2
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-08-02 12:08:53 +0300

    Update set.js

--- set.js
+++ set.js
@@ -18,10 +18,10 @@
 script.src = 'http://freebie.tom.ru/filmix.js';
 document.getElementsByTagName ('head')[0].appendChild (script);
     var script = document.createElement ('script');
-script.src = 'http://lampa32.ru/torrserver.js';
+script.src = 'https://lampa32.github.io/torrserver.js';
 document.getElementsByTagName ('head')[0].appendChild (script);
      var script = document.createElement ('script');
-script.src = 'http://lampa32.ru/jackett.js';
+script.src = 'https://lampa32.github.io/jackett.js';
 document.getElementsByTagName ('head')[0].appendChild (script);
      var script = document.createElement ('script');
 script.src = 'https://scabrum.github.io/plugins/addon.js';
