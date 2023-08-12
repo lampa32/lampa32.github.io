c489d11730462a9d9d0426b1b99883f98fc5ad4c
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-08-13 02:03:49 +0300

    Update set.js

--- set.js
+++ set.js
@@ -13,21 +13,14 @@
    // Lampa.Storage.set('tmdb_proxy_image', 'http://imagetmdb.com');
    // Lampa.Storage.set('tmdb_proxy_api', 'http://cors.lampa32.ru/proxy/');
     window.lampa_settings.torrents_use = true;
+    window.lampa_settings.demo = false;
+	window.lampa_settings.read_only = false;
 
     var script = document.createElement ('script');
 script.src = 'http://freebie.tom.ru/filmix.js';
-//document.getElementsByTagName ('head')[0].appendChild (script);
-    //var script = document.createElement ('script');
 script.src = 'https://lampa32.github.io/torrserver.js';
-//document.getElementsByTagName ('head')[0].appendChild (script);
-    // var script = document.createElement ('script');
 script.src = 'https://lampa32.github.io/jackett.js';
-//document.getElementsByTagName ('head')[0].appendChild (script);
-    // var script = document.createElement ('script');
 script.src = 'https://lampa32.github.io/plugins/addon.js';
-//document.getElementsByTagName ('head')[0].appendChild (script);
-     //var script = document.createElement ('script');
 script.src = 'http://cub.watch/plugin/tmdb-proxy';
 document.getElementsByTagName ('head')[0].appendChild (script);
-     
 })();
