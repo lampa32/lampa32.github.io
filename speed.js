7ed3466e67c61daa9263f414431ec7db7f961844
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-08-15 17:29:05 +0300

    Update speed.js

--- speed.js
+++ speed.js
@@ -2,19 +2,28 @@
     'use strict';
 Lampa.Platform.tv(); 
 Lampa.SettingsApi.addParam({
-	    		component: 'add_plugin',
+	    		component: 'Multi_Menu_Component',
 	    		param: {
-	    			name: 'SpeedTest',
+	    			name: 'OpenSpeedTestParam',
 	    			type: 'static', //доступно select,input,trigger,title,static
 	    		},
 	    		field: {
-	    			name: 'SpeedTest',
+	    			name: 'OpenSpeedTest',
 	    			description: 'Замер скорости интернет-соединения'
 	    		},
 	    		onRender: function (item) {
 	    			item.on('hover:enter', function(){
-	    			Lampa.Iframe.show({url: 'http://185.229.66.133:3000?XHR=1&R/',
-	              					onBack: function onBack() {Lampa.Controller.toggle('settings_component');}
-	            		});});}
+					var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;background-color: #ffffff;overflow:hidden !important;" src="http://185.229.66.133:3000?XHR=1&R"></iframe></div></div></div>');
+		  			Lampa.Modal.open({
+		  				title: '',
+		  				html: modal,
+		  				size: 'medium',
+		  				mask: true, 
+		  				onBack: function onBack() {
+		  					Lampa.Modal.close();
+							Lampa.Controller.toggle('settings_component');
+		  				},
+		  				onSelect: function () {}
+  			});});}
 				});
 })();
