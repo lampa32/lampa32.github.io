80be9257cf2b346f34708d3760df8af3d03269ec
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-08-15 17:17:20 +0300

    Update speed.js

--- speed.js
+++ speed.js
@@ -1,1 +1,20 @@
-
+(function () {
+    'use strict';
+Lampa.Platform.tv(); 
+Lampa.SettingsApi.addParam({
+	    		component: 'add_plugin',
+	    		param: {
+	    			name: 'SpeedTest',
+	    			type: 'static', //доступно select,input,trigger,title,static
+	    		},
+	    		field: {
+	    			name: 'SpeedTest',
+	    			description: 'Замер скорости интернет-соединения'
+	    		},
+	    		onRender: function (item) {
+	    			item.on('hover:enter', function(){
+	    			Lampa.Iframe.show({url: 'http://zamerka.com/',
+	              					onBack: function onBack() {Lampa.Controller.toggle('settings_component');}
+	            		});});}
+				});
+})();
