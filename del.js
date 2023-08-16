e0a53e972dc5aea92263d13f60f5252f036eb8d7
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-08-16 08:53:06 +0300

    Update del.js

--- del.js
+++ del.js
@@ -2,7 +2,7 @@
 	'use strict';
 Lampa.Platform.tv();
   if (!Lampa.Storage.get('delUpdated')) {
-     var updatedPlugins = pluginsArray.filter(function(obj) {return obj.url !== 'http://lampa32.ru/jackett.js'});
+     var updatedPlugins = pluginsArray.filter(function(obj) {return obj.url !== 'https://scabrum.github.io/plugins/jackett.js'});
      Lampa.Storage.set('plugins', updatedPlugins);
      Lampa.Storage.set('delUpdated', true); location.reload()
   }
