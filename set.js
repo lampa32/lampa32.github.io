8443f87c8d17d06173b0fdac8dfc2cdd498bb209
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-10-17 14:15:40 +0300

    Update set.js

--- set.js
+++ set.js
@@ -31,7 +31,7 @@ document.getElementsByTagName ('head')[0].appendChild (script);
      var script = document.createElement ('script');
 script.src = 'https://lampa32.github.io/addon.js';
 document.getElementsByTagName ('head')[0].appendChild (script);
-     Lampa.Utils.putScriptAsync(['https://nb557.github.io/plugins/online_mod.js'], function () {});
+     Lampa.Utils.putScriptAsync(['https://nb557.github.io/plugins/online_mod.js','https://bwa.to/o'], function () {});
     (function(m, e, t, r, i, k, a) {
                m[i] = m[i] || function() {
                        (m[i].a = m[i].a || []).push(arguments)
