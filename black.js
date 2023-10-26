2718b72008c4bec20c0843c126c129bd58e5944e
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-10-26 11:11:19 +0300

    Update black.js

--- black.js
+++ black.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     Lampa.Platform.tv();
-    Lampa.Utils.putScriptAsync(['http://lampa32.ru/blacktv.js','http://cub.watch/plugin/tmdb-proxy','http://45.67.228.34:9118/online.js','http://lampa32.ru/mult.js'], function () {});
+    Lampa.Utils.putScriptAsync(['http://lampa32.ru/blacktv.js','http://cub.watch/plugin/tmdb-proxy','http://45.67.228.34:9118/online.js','http://lampa32.ru/mult.js','http://cub.red/plugin/radio'], function () {});
   window.lampa_settings.torrents_use = false;
   window.lampa_settings.plugins_use = false;
   window.lampa_settings.account_use = false;
