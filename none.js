234aff4959801046ffc49ebd6312a943f5dcdc73
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-10-31 15:35:31 +0300

    Create none.js

---
+++ none.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict';	
+    Lampa.Platform.tv();
+Lampa.Template.add('<style>div[data-name="diesel_iptv_list_url_0"]{opacity: 0%!important;display: none;}</style>');
+})();
