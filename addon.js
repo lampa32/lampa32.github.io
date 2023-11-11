9fcc61c80129616ac6d56d589175bcca3bbe6b42
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-11-12 02:38:07 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -144,7 +144,7 @@ Lampa.SettingsApi.addComponent({
 				//$('body').append('<div id="hideInstall"><style>div.settings-param__value{opacity: 0%!important;display: none;}</style><div>')
 		                /* Сдвигаем раздел выше */
 				        setTimeout(function() {
-					  $('div[data-component=plugins]').before($('div[data-component=add_plugin]'))
+					  $('div[data-component=more]').before($('div[data-component=add_plugin]'))
 					}, 30)
 			}
 		});
