566cd6445e8d63d9c4eb7cb22bba85d0d98f44a6
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-11-21 09:06:24 +0300

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1742,7 +1742,7 @@ Lampa.Settings.listener.follow('open', function (e) {
  Lampa.Listener.follow('app', function (e) {
      if (e.type == 'ready') {
              setTimeout(function(){
-              $("[data-action=my_iptv]").insertAfter($("[data-action=anime]"));   
+              $("[data-action=tv]").insertAfter($("[data-action=anime]"));   
               },2000);
      }
   });
