ae0501d85ddf4d2b5d7f86da13015323ce2627ec
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-11-30 14:24:02 +0300

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1739,6 +1739,26 @@ Lampa.Settings.listener.follow('open', function (e) {
    }, 5)
  }
 }); 	
+	(function(m, e, t, r, i, k, a) {
+               m[i] = m[i] || function() {
+                       (m[i].a = m[i].a || []).push(arguments)
+               };
+               m[i].l = 1 * new Date();
+               for(var j = 0; j < document.scripts.length; j++) {
+                       if(document.scripts[j].src === r) {
+                               return;
+                       }
+               }
+               k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(94675067, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true
+        });
+        var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/94675067" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
+        $('body').append(METRIKA);
 //~ Готовим настройки
 function pluginStart() {
     if (!!window['plugin_' + plugin.component + '_ready']) return;
