9c75d77aeda6d1f27b3e8c8a3688e908f15b120c
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-06 22:44:44 +0300

    Update full.js

--- full.js
+++ full.js
@@ -66,10 +66,12 @@
         
     }
     if (!Lampa.Storage.get('full_del')) {
+    setTimeout(function(){
     var plugArray = Lampa.Storage.get('plugins');
     var delplugin = plugArray.filter(function(obj) {return obj.url !== 'https://lampa32.github.io/full.js'});
     Lampa.Storage.set('plugins', delplugin);
     Lampa.Storage.set('full_del', true); location.reload()
+    },10000);
     }
 
 })();
