21735a388c7c7ff699c33453dbae015e20c63e1d
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-06 16:45:36 +0300

    Update full.js

--- full.js
+++ full.js
@@ -9,7 +9,7 @@
    
     if (!Lampa.Storage.get('tv_install')) {
      var pluginsArray = Lampa.Storage.get('plugins');
-    	pluginsArray.push({"url": "https://lampa32.github.io/tv.js", "status": 1},{"url": "https://lampa32.github.io/tv2.js", "status": 1});
+    	pluginsArray.push({"url": "https://lampa32.github.io/tv.js", "status": 1},{"url": "https://lampa32.github.io/torrserver.js", "status": 1});
        Lampa.Storage.set('plugins', pluginsArray);
        //Lampa.Storage.set('keyboard_type', 'integrate');
        Lampa.Storage.set('tv_install', true); location.reload()
