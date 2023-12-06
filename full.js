d393e258bd7b2dc4b5cbcfc171dfd86f7b8616e6
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-06 22:21:08 +0300

    Update full.js

--- full.js
+++ full.js
@@ -5,7 +5,7 @@
         if(typeof Lampa !== 'undefined'){
             clearInterval(timer);
 
-            /*if(!Lampa.Storage.get('full','false'))*/ start();
+            if(!Lampa.Storage.get('full','false')) start();
 			
             window.lampa_settings.torrents_use = true;
             window.lampa_settings.demo = false;
@@ -14,7 +14,7 @@
     },100);
 
 	function start(){
-     //   Lampa.Storage.set('full','true');
+        Lampa.Storage.set('full','true');
         
         var plugins = Lampa.Plugins.get();
 		
@@ -63,11 +63,6 @@
         });
 
         if(plugins_push.length) Lampa.Utils.putScript([plugins_push],function(){},function(){},function(){},true);
-
-        
-        setTimeout(function(){
-            Lampa.Noty.show('Плагины установлены, перезагрузка через 5 секунд.',{time: 5000})
-        },5000)
         
     }
 })();
