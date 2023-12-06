339ca71d0f80559f3ce1100adb578a1bd841ba09
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-06 21:51:17 +0300

    Update full.js

--- full.js
+++ full.js
@@ -1,19 +1,51 @@
 (function () {
     'use strict';
-    Lampa.Platform.tv();
- 
-    Lampa.Storage.set('keyboard_type', 'integrate');
-    window.lampa_settings.torrents_use = true;
-    window.lampa_settings.demo = false;
-    window.lampa_settings.read_only = false;
-   
-    if (!Lampa.Storage.get('tv_install')) {
-     var pluginsArray = Lampa.Storage.get('plugins');
-    	pluginsArray.push({"url": "https://lampa32.github.io/torrserver.js", "status": 1});
-        //pluginsArray.push({"url": "https://lampa32.github.io/tv.js", "status": 1});
-       Lampa.Storage.set('plugins', pluginsArray);
-       //Lampa.Storage.set('keyboard_type', 'integrate');
-       Lampa.Storage.set('tv_install', true); location.reload()
-    }
+	
+    var timer = setInterval(function(){
+        if(typeof Lampa !== 'undefined'){
+            clearInterval(timer);
 
+            if(!Lampa.Storage.get('full','false')) start();
+			
+            window.lampa_settings.torrents_use = true;
+            window.lampa_settings.demo = false;
+            window.lampa_settings.read_only = false;
+        }
+    },100);
+
+	function start(){
+        Lampa.Storage.set('full','true');
+        Lampa.Storage.set('source','cub');
+        Lampa.Storage.set('parser_use','true');
+        Lampa.Storage.set('proxy_tmdb','false');
+        
+        Lampa.Storage.set('jackett_url','{jachost}');
+        Lampa.Storage.set('jackett_key','1');
+        Lampa.Storage.set('parser_torrent_type','jackett');
+
+        var plugins = Lampa.Plugins.get();
+
+        var plugins_add = [
+			{initiale}
+        ];
+
+        var plugins_push = []
+
+        plugins_add.forEach(function(plugin){
+            if(!plugins.find(function(a){ return a.url == plugin.url})){
+                Lampa.Plugins.add(plugin);
+                Lampa.Plugins.save();
+
+                plugins_push.push(plugin.url)
+            }
+        });
+
+        if(plugins_push.length) Lampa.Utils.putScript([plugins_push],function(){},function(){},function(){},true);
+
+        
+        setTimeout(function(){
+            Lampa.Noty.show('Плагины установлены, перезагрузка через 5 секунд.',{time: 5000})
+        },5000)
+        
+    }
 })();
