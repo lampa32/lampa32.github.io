2c9fe522bd135937b1346a7b8a3e78abdda143ad
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-10 16:44:51 +0300

    Update full.js

--- full.js
+++ full.js
@@ -93,7 +93,7 @@
 						default: true
                         		},
 					field: {
-                                name: '<div style="line-height: 1.4;">Надоело смотреть в плохом качестве?<br>Хочешь смотреть в FHD и 4K? Переходи в телеграм бот <span style="color: #24b4f9">@modssmy_bot</span> для подключения VIP</div>'
+                                name: ['<div style="padding: 1.5em 2em; padding-top: 0;">', '<div style="background: #3e3e3e; padding: 1em; border-radius: 0.3em;">', '<div style="line-height: 1.4;">РќР°РґРѕРµР»Рѕ СЃРјРѕС‚СЂРµС‚СЊ РІ РїР»РѕС…РѕРј РєР°С‡РµСЃС‚РІРµ?<br>РҐРѕС‡РµС€СЊ СЃРјРѕС‚СЂРµС‚СЊ РІ FHD Рё 4K? РџРµСЂРµС…РѕРґРё РІ С‚РµР»РµРіСЂР°Рј Р±РѕС‚ <span style="color: #24b4f9">@modssmy_bot</span> РґР»СЏ РїРѕРґРєР»СЋС‡РµРЅРёСЏ VIP</div>', '</div>', '</div>']
                         },
 		});
 })();
