c4fd3279c59e5094104c5521ae952e98541b15f1
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-10 15:55:05 +0300

    Update full.js

--- full.js
+++ full.js
@@ -84,7 +84,14 @@
       },6000);
     }
     var ads = '<div style="padding: 1.5em 2em; padding-top: 0;">', '<div style="background: #3e3e3e; padding: 1em; border-radius: 0.3em;">', '<div style="line-height: 1.4;">Надоело смотреть в плохом качестве?<br>Хочешь смотреть в FHD и 4K? Переходи в телеграм бот <span style="color: #24b4f9">@modssmy_bot</span> для подключения VIP</div>', '</div>', '</div>'
-    Lampa.SettingsApi.addParam({
+    Lampa.Settings.listener.follow('open', function (e) {
+					if (e.name == 'main') {
+						Lampa.SettingsApi.addComponent({
+							component: 'add_ads',
+							name: 'ads'
+						});
+					}
+	Lampa.SettingsApi.addParam({
 					component: 'add_plugin',
 					param: {
 						name: 'add_ads',
