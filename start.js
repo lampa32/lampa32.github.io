4530fe50ad347d8bdbb5523f10f95a77f4fb5b5d
lampa32 <140617338+lampa32@users.noreply.github.com>
2023-12-18 13:17:40 +0300

    Create start.js

---
+++ start.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+    Lampa.Platform.tv();
+ 
+  if (Lampa.Storage.get('source') == 'cub') {
+    Lampa.Storage.set('source', 'tmdb')
+  }
+
+    var plugArray = Lampa.Storage.get('plugins');
+    var delplugin = plugArray.filter(function(obj) {return obj.url !== 'http://cub.red/plugin/tmdb-proxy'});
+    Lampa.Storage.set('plugins', delplugin);
+
+    var pluginArray = Lampa.Storage.get('plugins');
+    var deleteplugin = pluginArray.filter(function(obj) {return obj.url !== 'https://cub.red/plugin/tmdb-proxy'});
+    Lampa.Storage.set('plugins', deleteplugin);
+
+
+    Lampa.TMDB.image = function (url) {
+      var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? 'http://cors.lampa32.ru/proxy/' + Lampa.Utils.addUrlComponent(base) : base;
+    };
+
+    Lampa.TMDB.api = function (url) {
+      var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? 'http://cors.lampa32.ru/proxy/' + Lampa.Utils.addUrlComponent(base) : base;
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+      if (e.name == 'tmdb') {
+        e.body.find('[data-parent="proxy"]').remove();
+      }
+    });
+
+    var dcma_timer = setInterval(function(){
+      if(window.lampa_settings.dcma){
+            clearInterval(dcma_timer)
+            window.lampa_settings.dcma = false
+      }
+    },1000)
+
+})();
