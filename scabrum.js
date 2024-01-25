ae1817646a990746ac025743ef1bd88fbfd1e6d6
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-01-25 12:26:52 +0300

    Create scabrum.js

---
+++ scabrum.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+  Lampa.Platform.tv();
+  Lampa.Settings.main().render().find('[data-component="plugins"]').unbind('hover:enter').on('hover:enter', function () {
+        Lampa.Extensions.show();
+        setTimeout(function (){
+          $('.extensions__item-url', Lampa.Extensions.render()).map(function (i, e){
+            if(e.textContent == 'https://scabrum.github.io/plugins/addon.js') $(e).html('💎').append('<span class="extensions__item-premium">VIP buy at @modssmy_bot</span>');
+          });
+        }, 500);
+      });
+})();
