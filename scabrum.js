780e702e52c0bc0f9c0eadc2820ab0ae641dc3fc
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-01-25 12:31:08 +0300

    Update scabrum.js

--- scabrum.js
+++ scabrum.js
@@ -5,7 +5,7 @@
         Lampa.Extensions.show();
         setTimeout(function (){
           $('.extensions__item-url', Lampa.Extensions.render()).map(function (i, e){
-            if(e.textContent == 'https://scabrum.github.io/plugins/addon.js') $(e).html('💎').append('<span class="extensions__item-premium">VIP buy at @modssmy_bot</span>');
+            //if(e.textContent == 'https://scabrum.github.io/plugins/addon.js') $(e).html('💎').append('<span class="extensions__item-premium">VIP buy at @modssmy_bot</span>');
           });
         }, 500);
       });
