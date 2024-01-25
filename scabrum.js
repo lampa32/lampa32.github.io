5b6d7771b340a2927dc85324f9ecda9bced65c8d
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-01-25 13:00:23 +0300

    Update scabrum.js

--- scabrum.js
+++ scabrum.js
@@ -1,15 +1,33 @@
 (function () {
     'use strict';
   Lampa.Platform.tv();
-  //Lampa.Settings.main().render().find('[data-component="plugins"]').unbind('hover:enter').on('hover:enter', function () {
-      //  Lampa.Extensions.show();
-        setTimeout(function (){
-           var plugArray = Lampa.Storage.get('plugins');
-           var delplugin = plugArray.filter(function(obj) {return obj.url !== 'https://scabrum.github.io/plugins/addon.js'});
-           Lampa.Storage.set('plugins', delplugin); 
-          //$('.extensions__item-url', Lampa.Extensions.render()).map(function (i, e){
-            //if(e.textContent == 'https://scabrum.github.io/plugins/addon.js') $(e).html('💎').append('<span class="extensions__item-premium">VIP buy at @modssmy_bot</span>');
-          //});
-        }, 500);
-    //  });
+  var updateplugins = false;
+    var plugins = Lampa.Storage.get('plugins','[]')
+    var updatedPlugins = plugins.filter(function(plug) 
+    {
+       if (plug.url.indexOf('scabrum.github.io') >= 0)
+       {
+          updateplugins = true;
+          return false;
+       }
+		
+       return true;
+    });
+
+    if (updateplugins)
+	{
+       Lampa.Storage.set('plugins', updatedPlugins);
+	   
+	   var checkResult = updatedPlugins.filter(function(obj) {return obj.url == 'http://skaztv.online/tv.js'});
+			if (checkResult=='') {
+				updatedPlugins.push({
+				"author": '@scabrum',
+				"url": 'http://skaztv.online/tv.js',
+				"name": 'TV by skaz',
+				"status": 1
+				});
+				Lampa.Storage.set('plugins', updatedPlugins);
+			}
+	}
+		
 })();
