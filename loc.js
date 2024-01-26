14244f2519446011197959ab33e0341ae3773177
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-01-26 20:54:05 +0300

    Update loc.js

--- loc.js
+++ loc.js
@@ -13,7 +13,7 @@ function startMe() {
 	$('#app > div.head > div > div.head__actions').append(domainBUTT);
 	$('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
 	//if (Lampa.Storage.get('location_server')) {
-	if (window.location.href == '') {
+	if (window.location.href == 'null') {
 	 setTimeout(function(){
           // $('#REDIRECT').remove()
 		 localStorage.removeItem("REDIRECT");
