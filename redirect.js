20764a7432a628cbf327e31b24f66e8bc9e8db81
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-02-01 14:55:32 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -17,8 +17,8 @@ function startMe() {
 				setTimeout(function(){
                                   $('#REDIRECT').remove()
                                 }, 10);
-		
 	}
+	
 	$('#REDIRECT').on('hover:enter hover:click hover:touch', function() {
 		window.location.href = server_protocol + Lampa.Storage.get('location_server')
 	});
