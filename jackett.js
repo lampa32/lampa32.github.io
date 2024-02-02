b24746723bf8b2dfb8234bc411bf2968351db3a2
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-02-02 16:56:28 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -118,7 +118,7 @@ Lampa.SettingsApi.addParam({
 					Lampa.Settings.update();
 				});
 				if(!Lampa.Storage.field('jackett_url_two') == 'no_parser') {
-					$('div[data-name="jackett_url"]').remove()
+					$('div[data-name="jackett_url"]').hide()
 				}
 				if(Lampa.Storage.field('parser_use')) item.show()&$('.settings-param__name', item).css('color','f3d900')&$('div[data-name="jackett_url_two"]').insertAfter('div[data-children="parser"]');
 				else item.hide();
