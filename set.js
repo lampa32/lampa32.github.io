d1535df28631e2f07d722808b53181abab81aa3c
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-02-11 17:24:29 +0300

    Update set.js

--- set.js
+++ set.js
@@ -31,7 +31,7 @@
     },1000)*/
 
 
-Lampa.Settings.listener.follow('open', function (e) {
+/*Lampa.Settings.listener.follow('open', function (e) {
  if (e.name == 'main') {
    setTimeout(function() {
     $('div[data-component="tmdb"]').remove();
@@ -60,7 +60,7 @@ Lampa.Settings.listener.follow('open', function (e) {
           $(".view--online", Lampa.Activity.active().activity.render()).empty().append('<svg viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32"><path d="m17 14.5 4.2-4.5L4.9 1.2c-.1-.1-.3-.1-.6-.2L17 14.5zM23 21l5.9-3.2c.7-.4 1.1-1 1.1-1.8s-.4-1.5-1.1-1.8L23 11l-4.7 5 4.7 5zM2.4 1.9c-.3.3-.4.7-.4 1.1v26c0 .4.1.8.4 1.2L15.6 16 2.4 1.9zM17 17.5 4.3 31c.2 0 .4-.1.6-.2L21.2 22 17 17.5z" fill="currentColor" fill="#ffffff" class="fill-000000"></path></svg>Онлайн');
        },10);
       }
-     })
+     })*/
        
     /* var script = document.createElement ('script');
 script.src = 'http://tv.lampa32.ru/online.js';
@@ -88,7 +88,6 @@ document.getElementsByTagName ('head')[0].appendChild (script);   */
     var deleteplugin = pluginArray.filter(function(obj) {return obj.url !== 'https://cub.red/plugin/tmdb-proxy'});
     Lampa.Storage.set('plugins', deleteplugin);*/
 	
-	
     (function(m, e, t, r, i, k, a) {
                m[i] = m[i] || function() {
                        (m[i].a = m[i].a || []).push(arguments)
@@ -109,4 +108,102 @@ document.getElementsByTagName ('head')[0].appendChild (script);   */
         });
         var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/94674961" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
         $('body').append(METRIKA);
+
+var initMarker = 0;
+	
+      function hideIT(){
+
+                document.addEventListener('DOMSubtreeModified', function removeAD(event){
+		  var cardElements = document.getElementsByClassName('card');
+		  if(cardElements.length > 0){
+			if (initMarker == 0) {
+			  initMarker = 1 // Флаг
+			  setTimeout(function(){
+ 				  $('.selectbox-item__lock').parent().css('display', 'none');
+				  $('.settings-param-title').last().css('display', 'none');
+			  }, 50)
+			  setTimeout(function(){
+				  initMarker = 0 // Снимаем флаг
+			  }, 500)
+			}
+		  }
+		}, false);
+		
+		var myCardInterval = setInterval(function(){
+			if (document.querySelector('.card') !== null) {
+				$('.card').on('hover:long', function () {
+						setTimeout(function(){	
+							$('.selectbox-item__lock').parent().css('display', 'none');
+							$('.settings-param-title').last().css('display', 'none');
+						},50)
+				})
+				clearInterval(myCardInterval);
+			 }
+		}, 100);
+		var myTextBoxInterval = setInterval(function(){
+			if (document.querySelector('.card__textbox') !== null) {
+				$('.card__textbox').parent().parent().remove();
+				clearInterval(myTextBoxInterval);
+			}
+		}, 100);
+	}
+	
+function cub_off() {
+
+	 setTimeout(function(){
+              $('.open--feed').remove();
+              $('.open--premium').remove();
+	      $('.open--notice').remove();
+          }, 1000);
+
+	  Lampa.Settings.listener.follow('open', function (e) {
+             if (e.name == 'account') {
+	        setTimeout(function(){
+		    $('.settings--account-premium').remove()
+		    $('div > span:contains("CUB Premium")').remove()
+		},0);
+             }
+          });
+	
+	  Lampa.Listener.follow('full', function(e) {
+                if (e.type == 'complite') {
+		  $('.button--book').on('hover:enter', function(){
+		    setTimeout(function(){	
+			$('.selectbox-item__lock').parent().css('display', 'none');
+			$('.settings-param-title').last().css('display', 'none');
+		    },0)
+		  });	  
+                   setTimeout(function(){
+			$('.button--subscribe').remove();
+		   },0);
+                }
+          })   
+  
+          Lampa.Storage.listener.follow('change', function (event) {
+               if (event.name == 'activity') {
+	              if (Lampa.Activity.active().component === 'bookmarks') {
+					$('.register:nth-child(4)').hide();
+					$('.register:nth-child(5)').hide();
+					$('.register:nth-child(6)').hide();
+					$('.register:nth-child(7)').hide();
+					$('.register:nth-child(8)').hide();
+		       }
+		       setTimeout(function(){
+			hideIT();
+		       }, 200)
+                }
+          });
+
+}	
+if(window.appready) cub_off();
+	else {
+		Lampa.Listener.follow('app', function(e) {
+			if(e.type == 'ready') {
+				cub_off(); hideIT();
+				$("[data-action=feed]").eq(0).remove();
+                                $("[data-action=subscribes]").eq(0).remove();
+				
+			}
+		});
+	}
 })();
