de2c1573ec594d18b8bc2156a985728c0d9d5ada
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-02-14 13:00:30 +0300

    Update set.js

--- set.js
+++ set.js
@@ -20,6 +20,12 @@
              Lampa.Storage.set('keyboard_type', 'integrate');
              Lampa.Storage.set('start_page', 'main');
              Lampa.Storage.set('source', 'cub');
+	     Lampa.Storage.set('animation', 'false');
+	     Lampa.Storage.set('mask', 'false');
+	     Lampa.Storage.set('player_normalization', 'true');
+	     Lampa.Storage.set('player_timecode', 'ask');
+	     Lampa.Storage.set('screensaver', 'false');
+	     Lampa.Storage.set('pages_save_total', '3');
     } 
 	
     Lampa.Utils.putScriptAsync([
