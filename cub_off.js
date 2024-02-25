faa3b8c68fc6a0e0e0256b57327a3e9e30bda36c
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-02-25 18:46:28 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -37,8 +37,8 @@
 				$('.card__textbox').parent().parent().remove();
 				clearInterval(myTextBoxInterval);
 			}
-			if (document.querySelector('.ad-bot') !== null) {
-				$('.ad-bot').parent().parent().remove();
+			if (document.querySelector('.ad-bot__text') !== null) {
+				$('.ad-bot__text').parent().parent().remove();
 				clearInterval(myTextBoxInterval);
 			}
 		}, 100);
