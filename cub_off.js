8501a2e988c8e7f72b3b06b0351cec25c389dc40
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-02 15:51:42 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -12,7 +12,7 @@
 			  initMarker = 1 // Флаг
 			  setTimeout(function(){
  				  $('.selectbox-item__lock').parent().css('display', 'none');
-				   if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {return} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+				   if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
 			  }, 50)
 			  setTimeout(function(){
 				  initMarker = 0 // Снимаем флаг
@@ -26,7 +26,7 @@
 				$('.card').on('hover:long', function () {
 						setTimeout(function(){	
 							$('.selectbox-item__lock').parent().css('display', 'none');
-							 if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {return} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+							 if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
 						},50)
 				})
 				clearInterval(myCardInterval);
@@ -72,7 +72,7 @@ function cub_off() {
 		  $('.button--book').on('hover:enter', function(){
 		    setTimeout(function(){	
 			$('.selectbox-item__lock').parent().css('display', 'none');
-			 if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {return} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+			 if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
 		    },0)
 		  });	  
                    setTimeout(function(){
