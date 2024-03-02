caa000f1624da2fde5519a88c4f65965cd5a66fa
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-02 18:43:25 +0300

    Update set.js

--- set.js
+++ set.js
@@ -76,7 +76,13 @@
 			  setTimeout(function(){
  				  $('.selectbox-item__lock').parent().css('display', 'none');
 				   if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
-				  //if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+				  /*if (document.querySelector("div > span > div > span")) {
+								if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {
+									return
+								} else {
+									if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
+								}
+							}*/
 			  }, 50)
 			  setTimeout(function(){
 				  initMarker = 0 // Снимаем флаг
@@ -91,7 +97,13 @@
 						setTimeout(function(){	
 							$('.selectbox-item__lock').parent().css('display', 'none');
 							 if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
-							 //if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+							 /*if (document.querySelector("div > span > div > span")) {
+								if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {
+									return
+								} else {
+									if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
+								}
+							}*/
 						},50)
 				})
 				clearInterval(myCardInterval);
@@ -143,7 +155,13 @@
 		    setTimeout(function(){	
 			$('.selectbox-item__lock').parent().css('display', 'none');
 			 if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
-			// if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {} else {if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');}
+			/*if (document.querySelector("div > span > div > span")) {
+								if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {
+									return
+								} else {
+									if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
+								}
+							}*/
 		    },0)
 		  });	  
                    setTimeout(function(){
