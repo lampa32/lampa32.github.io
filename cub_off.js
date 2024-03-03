771d1beeef94844b6dc74c8078ef7549dbef49f9
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-03 06:59:51 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -12,13 +12,13 @@
 			  initMarker = 1 // Флаг
 			  setTimeout(function(){
  				  $('.selectbox-item__lock').parent().css('display', 'none');
-				   /*if (document.querySelector("div > span > div > span")) {
+				   if (document.querySelector("div > span > div > span")) {
 								if (document.querySelector("div > span > div > span").innerText == '@lampa_plugins_uncensored') {
 									return
 								} else {
-								if (!$('.extensions__body').length)*/ $('.settings-param-title').last().css('display', 'none');
-								//}
-				  // }
+								if (!$('.extensions__body').length) $('.settings-param-title').last().css('display', 'none');
+								}
+				   }
 			  }, 50)
 			  setTimeout(function(){
 				  initMarker = 0 // Снимаем флаг
