03580931841db55f610bfee49e02dd36ff0c0d7a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-12 11:52:05 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,5 +1,21 @@
 (function () {
     'use strict';
+function _asyncToGenerator(fn) {
+    return function () {
+      var self = this,
+        args = arguments;
+      return new Promise(function (resolve, reject) {
+        var gen = fn.apply(self, args);
+        function _next(value) {
+          asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+        }
+        function _throw(err) {
+          asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+        }
+        _next(undefined);
+      });
+    };
+}
 function create() {
       var html;
       var timer;
