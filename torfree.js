4ade6fef0e465a45aff6dae4a212b364a9cf3e42
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-22 16:17:36 +0300

    Update torfree.js

--- torfree.js
+++ torfree.js
@@ -128,17 +128,16 @@ function searchRandom(need, number){
 						if($('div[data-name="torrserv"]').length > 1) item.hide();
 						$('.settings-param__name', item).css('color','ffffff');
 						$('div[data-name="torrserv"]').insertAfter('div[data-name="torrserver_use_link"]');
-						//Lampa.Controller.toggle('torrserv');
 						if (localStorage.getItem('torrserv') == '1') {
                                                    $('div[data-name="torrserver_url_two"]').hide()
 						   $('div[data-name="torrserver_url"]').hide()
-						   $('div[data-name="torrserver_use_link"]').hide()
+						   //$('div[data-name="torrserver_use_link"]').hide()
 						   $('div > span:contains("Ссылки")').remove()
-				                   Lampa.Controller.toggle('settings_param');
+				                   Lampa.Controller.toggle('settings_component');
                                                 }
 						if (localStorage.getItem('torrserv') == '0') {
 						   $('div[data-name="torrserver_url_two"]').hide()
-						   $('div[data-name="torrserver_use_link"]').hide()
+						  // $('div[data-name="torrserver_use_link"]').hide()
 				                   Lampa.Controller.toggle('settings_component');
 						}
 					}, 0);
