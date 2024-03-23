c26749fcb70e72e4c2d48dda85884f3ccce6ac6b
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-23 13:08:56 +0300

    Update torfree.js

--- torfree.js
+++ torfree.js
@@ -131,9 +131,9 @@ function searchRandom(need, number){
 						if (localStorage.getItem('torrserv') == '1') {
                                                    $('div[data-name="torrserver_url_two"]').hide()
 						   $('div[data-name="torrserver_url"]').hide()
-						   //$('div[data-name="torrserver_use_link"]').hide()
+						   $('div[data-name="torrserver_use_link"]').hide()
 						   $('div > span:contains("Ссылки")').remove()
-				                   Lampa.Controller.toggle('settings_component');
+				                   Lampa.Controller.focus(document.querySelector("#app > div.settings > div.settings__content.layer--height > div.settings__body > div > div > div > div > div:nth-child(3)"))
                                                 }
 						if (localStorage.getItem('torrserv') == '0') {
 						   $('div[data-name="torrserver_url_two"]').hide()
