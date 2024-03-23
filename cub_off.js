4ad291e42f2607a716916af859c17da136c8128c
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-23 15:38:07 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -142,7 +142,7 @@
                     $('.register:nth-child(7)').hide();
                     $('.register:nth-child(8)').hide();
                 }
-		if (Lampa.Activity.active().component === 'actor') {
+		if (Lampa.Activity.active().component ='actor') {
 			if ($('.button--subscribe'))
 			    setTimeout(function() {
 				$('.button--subscribe').hide()
