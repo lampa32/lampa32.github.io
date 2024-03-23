37ca387365a987add0a90903cab8054c7add9651
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-23 18:42:02 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -147,7 +147,7 @@
 			if ($('.button--subscribe'))
 			    setTimeout(function() {
 				$('.button--subscribe').hide();
-			    }, 500)
+			    }, 250)
 		}
                 // запускаем функцию сокрытия рекламы hideIT()
                 setTimeout(function() {
