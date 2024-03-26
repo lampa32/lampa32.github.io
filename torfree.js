4fa24985cf62bffeb34bd04de100cb5964fc7474
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-26 14:37:25 +0300

    Update torfree.js

--- torfree.js
+++ torfree.js
@@ -84,7 +84,7 @@ function myRequest(i) {
 		xhr.ontimeout = function() {
 				console.log("FreeTorr", 'Сервер ' + options[i] + ' не ответил');
 		}
-	}, 1000)
+	}, 3000)
 }
 
 function checkAlive() {
