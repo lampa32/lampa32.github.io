7e2b1828f6ce6675f28b0af8d172185484c2f320
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-26 14:17:46 +0300

    Update torfree.js

--- torfree.js
+++ torfree.js
@@ -93,7 +93,7 @@ function checkAlive() {
 	}
 }
 
-checkAlive();
+//checkAlive();
 
  var icon_switch_server = '<svg fill="currentColor" width="256px" height="256px" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <polygon points="22 23 13.83 23 16.41 20.41 15 19 10 24 15 29 16.41 27.59 13.83 25 22 25 22 23"></polygon> <polygon points="11 13 19.17 13 16.59 10.41 18 9 23 14 18 19 16.59 17.59 19.17 15 11 15 11 13"></polygon> <path d="M24.5,25H24V23h.5a5.4961,5.4961,0,0,0,.377-10.9795l-.8365-.0566-.09-.834a7.9979,7.9979,0,0,0-15.9014,0l-.09.834-.8365.0566A5.4961,5.4961,0,0,0,7.5,23H8v2H7.5A7.4964,7.4964,0,0,1,6.1782,10.124a9.9992,9.9992,0,0,1,19.6436,0A7.4964,7.4964,0,0,1,24.5,25Z" transform="translate(0 0)"></path> <rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" class="cls-1" width="32" height="32"></rect> </g></svg>'
  function switch_server() {
@@ -111,7 +111,7 @@ checkAlive();
 	
 	
 	$('#SWITCH_SERVER').on('hover:enter hover:click hover:touch', function() {
-		searchRandom();
+		start_free();
 		Lampa.Noty.show("Torrserver изменён")
 	});
    } 
@@ -229,11 +229,12 @@ checkAlive();
 			}
    });
 
-   if(window.appready) switch_server();
+   if(window.appready) {switch_server(); checkAlive();}
 	else {
 		Lampa.Listener.follow('app', function(e) {
 			if(e.type == 'ready') {
 				switch_server();
+				checkAlive();
 			}
 		});
 	}
