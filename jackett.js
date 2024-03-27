7b38615e1d9814d61babf3b9382c0d485b3eb826
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-27 18:11:57 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -121,9 +121,7 @@ Lampa.SettingsApi.addParam({
 					Lampa.Settings.update();
 				});
 				if (localStorage.getItem('jackett_urltwo') !== 'no_parser') {
-				  var M = document.querySelector("#app > div.settings > div.settings__content.layer--height > div.settings__body > div > div > div > div > div.focus > div:nth-child(2)")
-                                   Lampa.Controller.focus(M)
-                                   Lampa.Controller.toggle('settings_component')
+				  Lampa.Controller.focus(document.querySelector("#app > div.settings > div.settings__content.layer--height > div.settings__body > div > div > div > div > div > div:nth-child(2)"))
                                    $('div[data-name="jackett_url"]').hide()
                                    $('div[data-name="jackett_key"]').hide()
 				  // Lampa.Controller.toggle('settings_component');
