4c602da21450158ff1a409de4e3579971ba65e28
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-27 17:43:16 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -121,12 +121,12 @@ Lampa.SettingsApi.addParam({
 					Lampa.Settings.update();
 				});
 				if (localStorage.getItem('jackett_urltwo') !== 'no_parser') {
-				  var M = document.querySelector("#app > div.settings > div.settings__content.layer--height > div.settings__body > div > div > div > div > div > div:nth-child(2)")
+				  var M = document.querySelector("#app > div.settings > div.settings__content.layer--height > div.settings__body > div > div > div > div > div > div:nth-child(2) > div:nth-child(2)")
                                    Lampa.Controller.focus(M)
                                    Lampa.Controller.toggle('settings_component')
                                    $('div[data-name="jackett_url"]').hide()
                                    $('div[data-name="jackett_key"]').hide()
-				   Lampa.Controller.toggle('settings_component');
+				  // Lampa.Controller.toggle('settings_component');
                                 }
 				if(Lampa.Storage.field('parser_use')) item.show()&$('.settings-param__name', item).css('color','ffffff')&$('div[data-name="jackett_urltwo"]').insertAfter('div[data-name="parser_torrent_type"]');
 				else item.hide();
