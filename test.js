e15aa19d07df714e08210e2bf31f3544b739fbc4
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-29 00:33:41 +0300

    Update test.js

--- test.js
+++ test.js
@@ -138,13 +138,6 @@ function searchRandom(need, number){
                 }, 1000)
 	    }
         })    
-	Lampa.Storage.listener.follow('change', function(event) {
-            if (event.name == 'activity') {
-                if (Lampa.Activity.active().component != 'full') {
-                   $('#RELOAD').hide();
-		}
-	    }
-        });
 	  
 	var reloadSVG = icon_server_reload
 	var reloadBUTT = '<div id="RELOAD" class="head__action selector redirect-screen">' + reloadSVG + '</div>';
