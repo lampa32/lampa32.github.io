e81eed68cdce558ece6839242438a49eac5b2e43
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-03-31 17:35:54 +0300

    Update torfree.js

--- torfree.js
+++ torfree.js
@@ -190,6 +190,7 @@ function checkAlive() {
 			if (myResult !== 'undefined') Lampa.Storage.set('torrserver_url_two', 'http://' + myResult + ':8090');
 		    }, 3000) // без тайм-аута при старте приложения не успевает сформироваться новый массив
 		}
+		Lampa.Storage.set('switch_server_button', '1');
 	}
 
 /* Создаем параметр для выбора сервера */
