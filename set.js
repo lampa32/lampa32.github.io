40e770c36a8f0eb3b8f94113dc24187943a9078e
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-05 17:26:53 +0300

    Update set.js

--- set.js
+++ set.js
@@ -31,6 +31,7 @@
 	     Lampa.Storage.set('player_timecode', 'ask');
 	     Lampa.Storage.set('screensaver', 'false');
 	     Lampa.Storage.set('pages_save_total', '3');
+	     Lampa.Storage.set('switch_server_button', '2');
 	        location.reload()
     } 
 	
