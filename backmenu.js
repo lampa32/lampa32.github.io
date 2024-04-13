991c4048f27dbaf61e760a56db867a7c6b3fca5a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-13 15:58:28 +0300

    Update and rename tes.js to backmenu.js

---
+++ backmenu.js
@@ -0,0 +1,85 @@
+(function () {
+    'use strict';
+    Lampa.Platform.tv();
+
+function start() {
+
+       var stay = 0
+
+function closeApp() {
+    if (Lampa.Platform.is('apple_tv')) window.location.assign('exit://exit');
+    if (Lampa.Platform.is('tizen')) tizen.application.getCurrentApplication().exit();
+    if (Lampa.Platform.is('webos') && typeof window.close == 'function') window.close();
+    if (Lampa.Platform.is('android')) Android.exit();
+    if (Lampa.Platform.is('orsay')) Orsay.exit();
+    if (Lampa.Platform.is('netcast')) window.NetCastBack();
+    if (Lampa.Platform.is('browser')) window.close();
+}
+
+function showMeExitMenu() {
+  var enabled = Lampa.Controller.enabled().name;
+    var menu = [];
+
+    menu.push({
+  title:  'Выход'
+    });
+
+    menu.push({
+  title:  'Перезагрузить'
+    });
+
+    menu.push({
+  title:  'YouTube'
+    });
+
+    Lampa.Select.show({
+    title: 'Выход',
+    items: menu,
+    onBack: function onBack() {
+      stay = 0;
+      Lampa.Controller.toggle(enabled);
+    },
+    onSelect: function onSelect(a) {
+      stay = 0;
+      if (a.title == 'Выход') closeApp();
+      if (a.title == 'Перезагрузить') location.reload();
+      if (a.title == 'YouTube') window.location.href = 'https://youtube.com/tv';
+      //Lampa.Controller.toggle(enabled);
+    }
+    })
+}
+  
+  Lampa.Controller.listener.follow('toggle', function(e) {
+    if(e.name == 'select' && stay !== 1 && document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__head > div").innerText == Lampa.Lang.translate('title_out')) {
+      setTimeout(function() {
+        stay = 1
+        //window.history.back();
+        showMeExitMenu()
+      },10);
+    };
+  })
+
+	
+/*     Вместо пункта отмена, добавляем пункт
+        Перезагрузить при долгом тапе назад
+	
+Lampa.Controller.listener.follow('toggle', function(e) {
+    if(e.name == 'select') {
+      setTimeout(function() {
+        if (document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__head > div").innerText == Lampa.Lang.translate('title_out')) {
+          document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__body.layer--wheight > div > div > div > div:nth-child(2) > div").text('Перезагрузить')
+          $('.selectbox-item')[1].on('hover:enter hover:click hover:touch', function() {
+            location.reload();
+          });
+        }
+      }, 100);
+    }
+  });  */
+	
+	
+} 
+  
+if(window.appready) start();
+else Lampa.Listener.follow('app', function(e) {	if(e.type == 'ready') start(); });
+
+})();
