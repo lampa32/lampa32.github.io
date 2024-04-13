629e0da5793318d2580fb89db7139f3f076a672a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-13 22:06:00 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -15,7 +15,7 @@ function showServerInput() {
       }, function (value) {
         // здесь редирект;
 	window.location.href = server_protocol + value;
-        if (value == '')  stay = 0;
+        if (value == '')  return;
           })
 }	
 
