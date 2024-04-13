4c3b289f15b0e315bfab033c894699c6d13aef0b
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-13 17:32:12 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -37,7 +37,7 @@ function showMeExitMenu() {
     items: menu,
     onBack: function onBack() {
       stay = 0;
-      Lampa.Controller.toggle(enabled);
+      //Lampa.Controller.toggle(enabled);
     },
     onSelect: function onSelect(a) {
       stay = 0;
