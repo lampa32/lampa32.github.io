6329c7abc47e1c560527b4a5a6c48ece20a29997
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-25 19:54:52 +0300

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1365,7 +1365,7 @@ function langGet(name) {
 
 langAdd('default_playlist',
     {
-	ru: 'http://troya.one/pl/3/6jozt90cr7szx/playlist.m3u8',
+	ru: 'http://79.137.204.8/free.m3u8',
 	//ru: 'https://raw.githubusercontent.com/blackbirdstudiorus/LoganetXIPTV/main/LoganetXAll.m3u',
 	uk: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
 	be: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
