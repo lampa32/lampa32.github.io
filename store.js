ef90763c93b8c3d07eb18617d2831e1b0083c402
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-29 08:22:49 +0300

    Update store.js

--- store.js
+++ store.js
@@ -26,7 +26,7 @@
                         onRender: function(item) {
 			     setTimeout(function() {
 				   //  item.show();
-				$('.settings-param__static', item).before($('div[data-name="interface_size"]'));
+				$('div[data-name="interface_size"]').before($('.settings-param__title', item));
 			     }, 100);
                               item.on('hover:enter', function () {
                                   Lampa.Extensions.show({ store: 'http://skaztv.online/js/extensions.json' });
