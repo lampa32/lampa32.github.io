43416144b7fc2abc76b0d8e10261e9c4eca34e59
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-30 01:32:48 +0300

    Update store.js

--- store.js
+++ store.js
@@ -1,27 +1,51 @@
 (function () {
     'use strict';
                
+let ThemeManager = (function() {
+  let currentTheme = null;
+
+  function init(themeData) {
+    if (themeData && themeData.themes && themeData.themes.length > 0) {
+      applyTheme(themeData.themes[0]); // Применяем первую тему из JSON
+    }
+  }
+
+  function applyTheme(themeData) {
+    removeCurrentTheme();
+
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = themeData.link;
+    link.id = 'current-theme';
+    head.appendChild(link);
+    currentTheme = themeData;
+    console.log(`Тема "${themeData.name}" успешно применена.`);
+  }
+
+  function removeCurrentTheme() {
+    const currentThemeLink = document.getElementById('current-theme');
+    if (currentThemeLink) {
+      currentThemeLink.remove();
+      currentTheme = null;
+    }
+  }
+
+  return {
+    init: init,
+    applyTheme: applyTheme,
+    removeCurrentTheme: removeCurrentTheme
+  };
+})();
+
+// Использование ThemeManager
 fetch('https://lampa32.github.io/extensions.json')
   .then(response => response.json())
-  .then(data => {
-    const themesData = data.themes;
-
-    function applyTheme(themeData) {
-      const head = document.getElementsByTagName('head')[0];
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.type = 'text/css';
-      link.href = themeData.link;
-      head.appendChild(link);
-      console.log(`Тема "${themeData.name}" успешно установлена.`);
-    }
-
-    // Применение всех тем из JSON-данных
-    themesData.forEach(themeData => {
-      applyTheme(themeData);
-    });
-
-    // Ваш код
+  .then(themeData => {
+    ThemeManager.init(themeData);
+    
+    // Добавление нового параметра настроек
     Lampa.SettingsApi.addParam({
       component: 'interface',
       param: {
