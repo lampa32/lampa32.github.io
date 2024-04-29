901ac4484faa6dc2160d225ebc8c53a4ef0a1060
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-29 15:26:05 +0300

    Update store.js

--- store.js
+++ store.js
@@ -25,10 +25,43 @@
 			},
                         onRender: function(item) {
 			    setTimeout(function() {
-        const targetElement = $('div[data-name="interface_size"]');
-        if (targetElement.length) {
-            $('.settings-param__name', item).insertAfter(targetElement);
-        }
+        // Получаем ссылку на элемент, который нужно переместить
+const paramName = document.querySelector("#app > div.settings > div.settingscontent.layer--height > div.settingsbody > div > div > div > div > div:nth-child(23) > div");
+
+// Создаем временный контейнер
+const tempContainer = document.createElement("div");
+tempContainer.style.position = "fixed";
+tempContainer.style.top = "50%";
+tempContainer.style.left = "50%";
+tempContainer.style.transform = "translate(-50%, -50%)";
+tempContainer.style.zIndex = "9999";
+
+// Клонируем элемент и помещаем клон в временный контейнер
+const paramClone = paramName.cloneNode(true);
+tempContainer.appendChild(paramClone);
+
+// Добавляем временный контейнер на страницу
+document.body.appendChild(tempContainer);
+
+// Удаляем старый элемент
+paramName.parentNode.removeChild(paramName);
+
+// Обработчик события клика на клонированном элементе
+paramClone.addEventListener("click", () => {
+  console.log("Элемент был нажат");
+});
+
+// Ждем полной загрузки страницы
+window.addEventListener("load", () => {
+  // Получаем ссылку на целевой элемент, в который нужно переместить параметр
+  const targetElement = document.querySelector("#target-element");
+
+  // Перемещаем клонированный элемент в целевой элемент
+  targetElement.appendChild(paramClone);
+
+  // Удаляем временный контейнер
+  tempContainer.parentNode.removeChild(tempContainer);
+});
     }, 100);
                               item.on('hover:enter', function () {
                                   Lampa.Extensions.show({ store: 'http://skaztv.online/js/extensions.json',with_installed: true });
