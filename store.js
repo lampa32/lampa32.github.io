bfcada73a6ce99cebf2f4f8a73166f415194a9e1
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-04-29 05:38:54 +0300

    Update store.js

--- store.js
+++ store.js
@@ -25,7 +25,7 @@
                         },
                         onRender: function(item) {
 			     setTimeout(function() {
-				$(.settings-param__name).insertAfter($('div[data-name="interface_size"]'));
+				$('div[data-name="col"]').insertAfter($('div[data-name="interface_size"]'));
 			     }, 100);
                               item.on('hover:enter', function () {
                                   Lampa.Extensions.show({ store: 'http://skaztv.online/js/extensions.json' });
