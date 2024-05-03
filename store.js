a696d2862d45aaa30c365831c4d24728a3dde0f0
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-03 21:14:28 +0300

    Update store.js

--- store.js
+++ store.js
@@ -4,7 +4,7 @@
     Lampa.Controller.listener.follow('toggle', function(e) {
     if (e.name === 'select') {
         setTimeout(function() {
-            $('.item-name > div:contains("White")')
+            $('.extensions__item-name > div:contains("White")')
                 setTimeout(function() {
                    // $('.selectbox-item > div:contains("Включить")').click(function() {
                         var link = document.createElement('link');
