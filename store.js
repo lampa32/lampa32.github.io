572371960970e2dd96aaaf2f0ad2dd75b764e12f
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-04 10:10:20 +0300

    Update store.js

--- store.js
+++ store.js
@@ -4,7 +4,7 @@
 Lampa.Controller.listener.follow('toggle', function(e) {
     if(e.name == 'select') {
         setTimeout(function() {
-            $('.extensions__item-name > div:contains("White")').length && $('.selectbox-item > div:contains("Включить")').on('click', function() {
+            $('.extensions__item-name > div:contains("White")') && $('.selectbox-item > div:contains("Включить")').on('click', function() {
                 var link = document.createElement('link');
                 link.rel = 'stylesheet';
                 link.href = 'http://lampa.run.place/copenhagen.css';
