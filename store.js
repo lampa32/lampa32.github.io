17bc249060f5e221a75e41cef632fa012005a290
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-08 11:47:49 +0300

    Update store.js

--- store.js
+++ store.js
@@ -34,14 +34,15 @@ Lampa.Controller.listener.follow('toggle', function(e) {
       if (localStorage.getItem('myTheme') == 'Copenhagen') {
           console.log('Обработчик для темы Copenhagen (Отключить)');
         //$('.selectbox-item > div:contains("Отключить")').on('click', function() {
-           setTimeout(function() {
-          $('.selectbox-item > div:contains("Отключить")').on('click', function() {
+          var disableButtons = document.querySelectorAll('.selectbox-item > div:contains("Отключить")');
+disableButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
              console.log('Клик на кнопке "Отключить" для темы Copenhagen');
           $('link[rel="stylesheet"][href^="http://lampa.run.place/"]').remove();
           localStorage.setItem('myTheme', 'Disabled');
           $('.selectbox-item > div:contains("Отключить")').onclick = null;
-        });
-               }, 100);
+        })
+})
       }
       if (localStorage.getItem('myTheme') == 'Authentic Brief') {
         $('.selectbox-item > div:contains("Включить")').on('click', function() {
