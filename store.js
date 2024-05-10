ff9862d89397b5c0a426944e55eb2bd2228f0249
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-10 16:31:41 +0300

    Update store.js

--- store.js
+++ store.js
@@ -24,12 +24,12 @@ Lampa.Controller.listener.follow('toggle', function(e) {
     setTimeout(function() {
       if (localStorage.getItem('myTheme') == 'Copenhagen') {
           console.log('Обработчик для темы Copenhagen');
-        $('.selectbox-item > div:contains("Включить")')//.on('click', function() {
+        $('.selectbox-item > div:contains("Включить")').on('click', function() {
             console.log('Клик на кнопке "Включить" для темы Copenhagen');
           $('link[rel="stylesheet"][href^="http://lampa.run.place/"]').remove();
           loadThemeCSS('copenhagen');
-         // $('.selectbox-item > div:contains("Включить")').onclick = null;
-        //});
+          $('.selectbox-item > div:contains("Включить")').onclick = null;
+        });
       }
       if (localStorage.getItem('myTheme') == 'Copenhagen') {
           console.log('Обработчик для темы Copenhagen (Отключить)');
