837a1b4d8811d4900f5c2abb24aa0bc8e933d7f5
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-13 07:28:11 +0300

    Update j.js

--- j.js
+++ j.js
@@ -101,7 +101,9 @@ function pollParsers(menu) {
      function myRequest(url, title, menuItem) {
         return new Promise(function(resolve, reject) {
         var proto = location.protocol === "https:" ? 'https://' : 'http://';
-        var myLink = proto + url + '/api/v2.0/indexers/status:healthy/results?apikey=' + (menuItem.jac_key ? '&' + menuItem.jac_key : '');
+        var myAdder = '';
+        if (url == 'spawn.pp.ua:59117') var myAdder = '2'
+        var myLink = proto + url + '/api/v2.0/indexers/status:healthy/results?apikey=' + myAdder;//(menuItem.jac_key ? '&' + menuItem.jac_key : '');
 
         var xhr = new XMLHttpRequest();
         xhr.open('GET', myLink, true);
