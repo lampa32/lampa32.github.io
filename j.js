01bee03b4f7dd9c0aeffe290693a2e81c7446e4e
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-13 08:52:29 +0300

    Update j.js

--- j.js
+++ j.js
@@ -10,7 +10,10 @@
     menu.push({
         title: 'Lampa32',
         url: 'jac.lampa32.ru',
-        jac_key: ''
+        jac_key: '',
+        jac_int: 'all',
+        jac_search: true,
+        jac_lang: 'lg'
     });
 
     menu.push({
@@ -70,7 +73,7 @@
                     Lampa.Controller.toggle(enabled);
                 },
                 onSelect: function onSelect(a) {
-                    Lampa.Storage.set('jackett_url', a.url) & Lampa.Storage.set('jackett_key', a.jac_key) & Lampa.Storage.set('jackett_interview', 'all') & Lampa.Storage.set('parse_in_search', true) & Lampa.Storage.set('parse_lang', 'lg');
+                    Lampa.Storage.set('jackett_url', a.url) & Lampa.Storage.set('jackett_key', a.jac_key) & Lampa.Storage.set('jackett_interview', a.jac_int) & Lampa.Storage.set('parse_in_search', a.jac_search) & Lampa.Storage.set('parse_lang', a.jac_lang);
                     Lampa.Controller.toggle(enabled);
                     var activ = Lampa.Storage.get('activity')
                     setTimeout(function() {
