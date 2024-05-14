8b98f8e498d8912547b0aefd49ed781015d6340b
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 17:06:46 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -2,15 +2,26 @@
     'use strict';
     Lampa.Platform.tv();
 
- /* Lampa.Listener.follow('full', function(e) {
+Lampa.Listener.follow('full', function(e) {
+  if (e.type == 'complite') {
+    setTimeout(function() {
+      $('.card').each(function() {
+        const $card = $(this);
+        const $firstButton = $card.find('.view--torrent').first();
+        $firstButton.prependTo($card);
+      });
+    }, 10);
+  }
+});
+	
+  /*Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
        setTimeout(function() {
           $('.view--torrent').first().prependTo($('.view--torrent').first().parent());
-         // if ($('.view--torrent').length > 1) $('.view--torrent')[1].remove();
        },10);
      }
   })*/
-    let isButtonMoved = false;
+   /* let isButtonMoved = false;
 
 Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
@@ -24,7 +35,7 @@ Lampa.Listener.follow('full', function(e) {
   } else if (e.type == 'complite') {
     isButtonMoved = false;
   }
-});
+});*/
    /* Lampa.Controller.listener.follow('toggle', function(e) {
 			  if (e.name == 'select') {
                     if (Lampa.Activity.active().component == 'full') {
