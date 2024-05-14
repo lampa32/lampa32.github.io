df5a9a666f5a586b124d47f76b11b9b2a9e08cc1
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 16:09:57 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -10,20 +10,31 @@
        },10);
      }
   })*/
-    let isButtonMoved = false;
+    /*let isButtonMoved = false;
 
 Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
       if (!isButtonMoved) {
         $('.view--torrent').first().prependTo($('.view--torrent').first().parent());
-       // if ($('.view--torrent').length > 1) $('.view--torrent').slice(1).remove();
+        if ($('.view--torrent').length > 1) $('.view--torrent').slice(1).remove();
         isButtonMoved = true;
       }
     }, 10);
-  } else /*if (e.type == 'start')*/ {
+  } else if (e.type == 'start') {
     isButtonMoved = false;
   }
-});
+});*/
+    Lampa.Controller.listener.follow('toggle', function(e) {
+			  if (e.name == 'select') {
+                    if (Lampa.Activity.active().component == 'full') {
+                      setTimeout(function() {
+                        $('.view--torrent').first().prependTo($('.view--torrent').first().parent());
+                      }, 10)
+                    }
+              }
+    });
+                
+                    
   
 })();
