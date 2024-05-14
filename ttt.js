c22f986df8dab299124011a0acd6ec4e43279812
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 15:47:13 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -2,13 +2,28 @@
     'use strict';
     Lampa.Platform.tv();
 
-  Lampa.Listener.follow('full', function(e) {
+ /* Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
        setTimeout(function() {
           $('.view--torrent').first().prependTo($('.view--torrent').first().parent());
          // if ($('.view--torrent').length > 1) $('.view--torrent')[1].remove();
        },10);
      }
-  })
+  })*/
+    let isButtonMoved = false;
+
+Lampa.Listener.follow('full', function(e) {
+  if (e.type == 'complite') {
+    setTimeout(function() {
+      if (!isButtonMoved) {
+        $('.view--torrent').first().prependTo($('.view--torrent').first().parent());
+        if ($('.view--torrent').length > 1) $('.view--torrent').slice(1).remove();
+        isButtonMoved = true;
+      }
+    }, 10);
+  } else if (e.type == 'start') {
+    isButtonMoved = false;
+  }
+});
   
 })();
