ab78bf9dedb18a9d3a4511df0570f3d40097b750
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 14:16:31 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -5,7 +5,7 @@
   Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
        setTimeout(function() {
-         $('.view--torrent').appendTo('body');
+         $('.view--torrent').last();
          // if ($('.view--torrent').length > 1) $('.view--torrent')[1].remove();
        },10);
      }
