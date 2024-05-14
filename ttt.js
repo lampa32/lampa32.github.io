fc754ab271889c02ab8a0d9e8cdbab3e9f98fa90
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 23:08:33 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -2,7 +2,7 @@
     'use strict';
     Lampa.Platform.tv();
 
-Lampa.Listener.follow('full', function(e) {
+/*Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
       $('.view--online').each(function() {
@@ -30,6 +30,45 @@ Lampa.Listener.follow('full', function(e) {
 
         if (!$torrentButton.is($thirdChild)) {
           $torrentButton.insertAfter($parent.children().eq(1)); // Вставляем $torrentButton после второго элемента
+        }
+      });
+    }, 10);
+  }
+});*/
+	Lampa.Listener.follow('full', function(e) {
+  if (e.type == 'complite') {
+    // Вставляем кнопки перед кнопкой "Воспроизвести"
+    insertBefore($('.button--play'));
+
+    setTimeout(function() {
+      // Код для упорядочивания кнопок
+      $('.view--online').each(function() {
+        var $torrentButton = $(this);
+        var $parent = $torrentButton.parent();
+        var $firstChild = $parent.children().first();
+
+        if (!$torrentButton.is($firstChild)) {
+          $torrentButton.prependTo($parent);
+        }
+      });
+
+      $('.view--online_mod').each(function() {
+        var $onmodButton = $(this);
+        var $parent = $onmodButton.parent();
+        var $secondChild = $parent.children().eq(1);
+
+        if (!$onmodButton.is($secondChild)) {
+          $onmodButton.insertBefore($secondChild);
+        }
+      });
+
+      $('.view--torrent').each(function() {
+        var $torrentButton = $(this);
+        var $parent = $torrentButton.parent();
+        var $thirdChild = $parent.children().eq(2);
+
+        if (!$torrentButton.is($thirdChild)) {
+          $torrentButton.insertAfter($parent.children().eq(1));
         }
       });
     }, 10);
