fd6d9825ca0ad3e4ff15c4ccda05164298def381
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-14 18:06:12 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -14,7 +14,7 @@ Lampa.Listener.follow('full', function(e) {
           $torrentButton.prependTo($parent);
         }
       });
-    }, 150);
+    }, 120);
   }
 });
   /*Lampa.Listener.follow('full', function(e) {
