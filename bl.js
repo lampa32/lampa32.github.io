00973a5a59129c742b5c7399ff528702e3946603
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-15 17:19:34 +0300

    Update bl.js

--- bl.js
+++ bl.js
@@ -2,31 +2,47 @@
     'use strict';
     Lampa.Platform.tv();
 
-     // Ждем, пока функция load$2 будет определена
-    function waitForLoad$2() {
-        if (typeof window.load$2 === 'function') {
-            patchLoad$2();
+       // Ждем, пока функция loadBlackList будет определена
+    function waitForLoadBlackList() {
+        if (typeof window.loadBlackList === 'function') {
+            patchLoadBlackList();
         } else {
-            setTimeout(waitForLoad$2, 100);
+            setTimeout(waitForLoadBlackList, 100);
         }
     }
 
-    function patchLoad$2() {
-        // Сохраняем оригинальную функцию load$2
-        var originalLoad$2 = window.load$2;
+    function patchLoadBlackList() {
+        // Сохраняем оригинальную функцию loadBlackList
+        var originalLoadBlackList = window.loadBlackList;
 
-        // Переопределяем функцию load$2
-        window.load$2 = function (call) {
-            // Вызываем оригинальную функцию load$2
-            originalLoad$2.call(this, call);
+        // Создаем прокси для функции loadBlackList
+        window.loadBlackList = new Proxy(originalLoadBlackList, {
+            apply: function (target, thisArg, args) {
+                // Вызываем оригинальную функцию loadBlackList
+                return Reflect.apply(target, thisArg, args);
+            },
+            construct: function (target, args) {
+                // Вызываем оригинальную функцию loadBlackList
+                return new target(...args);
+            }
+        });
 
-            // После выполнения оригинальной функции, фильтруем черный список
-            _blacklist = _blacklist.filter(function (url) {
-                return !url.includes('scabrum.github.io');
-            });
-        }
+        // Перехватываем вызов функции-обратного вызова (callback) внутри loadBlackList
+        var originalCall = window.loadBlackList;
+        window.loadBlackList = function (call) {
+            // Создаем новую функцию-обратный вызов, которая фильтрует черный список
+            var filteredCall = function (blackList) {
+                blackList = blackList.filter(function (url) {
+                    return !url.includes('scabrum.github.io');
+                });
+                call(blackList);
+            };
+
+            // Вызываем оригинальную функцию loadBlackList с новым обратным вызовом
+            originalCall(filteredCall);
+        };
     }
 
-    waitForLoad$2();
+    waitForLoadBlackList();
     
 })()
