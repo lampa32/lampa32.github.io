f8b223e82d0aa0f7d8e215dfae4c1bc0ec967cbb
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-15 08:15:37 +0300

    Create ttt.js

---
+++ ttt.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+Lampa.Listener.follow('full', function(e) {
+  if (e.type == 'complite') {
+    setTimeout(function() {
+      Lampa.Activity.active().activity.render().find('.button--play').before('.view--torrent');
+    }, 10);
+  }
+});
+ })();
