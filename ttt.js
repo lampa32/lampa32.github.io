16aa8a3d34bc46cfd61e1c42f60e6529a7fe3e5a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-15 08:25:58 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -3,7 +3,10 @@
 Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
-      Lampa.Activity.active().activity.render().find('.button--play').before($('.view--torrent'));
+      // Удаляем все клоны перед вставкой нового
+      $('.view--torrent.clone').remove();
+
+      Lampa.Activity.active().activity.render().find('.button--play').before($('.view--torrent').clone());
     }, 10);
   }
 });
