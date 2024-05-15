c82a736f0069a44f47b85e8bcf45a79c2b311b96
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-15 16:27:34 +0300

    Update bl.js

--- bl.js
+++ bl.js
@@ -3,33 +3,11 @@
     Lampa.Platform.tv();
 
     function loadBlackList(call) {
-    var status = new status$2(2);
+        
+    // Игнорируем загрузку черных списков
+    // и сразу вызываем call с пустым массивом
+    call([]);
+}
 
-    status.onComplite = function (res) {
-        // Объединяем списки cub и custom
-        let combinedList = [].concat(res.cub, res.custom);
-
-        // Фильтруем список, исключая scabrum.github.io
-        const modifiedList = combinedList.filter(url => !url.includes('scabrum.github.io'));
-
-        call(modifiedList);
-    };
-
-    _network.silent(Utils$2.protocol() + object$2.cub_domain + '/api/plugins/blacklist', function (result) {
-        var list = result.map(function (a) {
-            return a.url;
-        });
-        Storage.set('plugins_blacklist', list);
-        status.append('cub', list);
-    }, function () {
-        status.append('cub', Storage.get('plugins_blacklist', '[]'));
-    });
-
-    _network.silent('./plugins_black_list.json', function (list) {
-        status.append('custom', list);
-    }, function () {
-        status.append('custom', []);
-    });
-    }
     
 })()
