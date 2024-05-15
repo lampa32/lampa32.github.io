71e88c0a81d32570d8e741dd3b82349045c59eca
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-15 08:55:17 +0300

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -4,9 +4,21 @@ Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
       var container = Lampa.Activity.active().activity.render();
-      container.find('.view--torrent').not(':first').remove(); // Удаление дубликатов
-      var firstTorrent = container.find('.view--torrent').first(); // Получение первого элемента
-      firstTorrent.clone().insertBefore(container.find('.button--play')); // Клонирование и вставка перед .button--play
+      
+      // Удаление предыдущих клонированных элементов перед .button--play
+      container.find('.button--play').prevAll('.view--torrent').remove();
+      
+      // Получение первого элемента .view--torrent
+      var firstTorrent = container.find('.view--torrent').first();
+      
+      // Клонирование первого элемента .view--torrent и вставка перед .button--play
+      var clonedTorrent = firstTorrent.clone().insertBefore(container.find('.button--play'));
+      
+      // Удаление изначального элемента .view--torrent из списка
+      firstTorrent.remove();
+      
+      // Удаление дубликатов (на всякий случай)
+      container.find('.view--torrent').not(clonedTorrent).remove();
     }, 10);
   }
 });
