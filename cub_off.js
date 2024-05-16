6892bb38e6fd5739697d4661c85df32895b5417a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-16 13:10:38 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -132,9 +132,7 @@
                 });
 		$('.full-person').on('hover:enter', function() {
                     // убираем кнопку подписаться в карточке актера
-			setTimeout(function() {
-				$('.button--subscribe').hide();
-			}, 350)
+			$('.button--subscribe').remove();
                 });
                 // скрываем кнопку ПОДПИСАТЬСЯ в карточке
 		    setTimeout(function() {
