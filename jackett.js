da3a17d7572fbb174d651bb330e6eb872707da3d
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-17 11:09:55 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -53,7 +53,7 @@ xhr.onload = function() {
     if (xhr.status == 200) {
         if ($(mySelector).text() == parserName[i]) $(mySelector).html('&#10003;&nbsp;&nbsp;' + $(mySelector).text()).css('color', '1aff00');
     }
-    if (xhr.status == 401) {
+    if (xhr.status == 502) {
         if ($(mySelector).text() == parserName[i]) $(mySelector).html('&#10008;&nbsp;&nbsp;' + $(mySelector).text()).css('color', 'ff2e36');
     }
 }
