02252d3e938743da7f08e6adf503256c499258dc
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-17 10:07:22 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -283,7 +283,7 @@ function myMenuRequest(url, title, menuItem) {
 
         var xhr = new XMLHttpRequest();
         xhr.open('GET', myLink, true);
-        xhr.timeout = 14000;
+        xhr.timeout = 3000;
 
         xhr.onload = function() {
            // console.log('Response Status:', xhr.status);
