0f9df9833b9cc294b93453fc42f6d96e9301907f
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-17 08:40:39 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -137,7 +137,7 @@
                         }
                 }*/
 
-                       // Удаляем кнопку "Subscribe" при наведении на .full-person
+                      /* // Удаляем кнопку "Subscribe" при наведении на .full-person
                 $('.full-person').on('hover:enter', function() {
 			var subBut = setInterval(function() {
                            if($('.button--subscribe').length) {
@@ -146,14 +146,14 @@
                               //clearInterval(subBut);
 			   }
 			}, 5);		
-		});
+		});*/
 
-                       	  /*  // скрываем кнопку Подписаться в карточке актёра 
+                       	    // скрываем кнопку Подписаться в карточке актёра 
 		$('.full-person').on('hover:enter', function() {
 		       setTimeout(function() {
 			    $('.button--subscribe').remove();
 		       }, 350); 
-		});*/
+		});
                 // скрываем кнопку ПОДПИСАТЬСЯ в карточке
 		setTimeout(function() {
                         $('.button--subscribe').remove();
