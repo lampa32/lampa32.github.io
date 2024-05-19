15b5817aa91e6df606eff85a67227f2aa358b45b
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-19 10:30:46 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -53,8 +53,7 @@ function myRequest(i) {
                    if (xhr.status == 200) {
                        //if ($(mySelector).text() == parserName[i]) $(mySelector).html('&#10004;&nbsp;&nbsp;' + $(mySelector).text()).css('color', '1aff00');
                        if ($(mySelector).text() == parserName[i]) {
-    //$(mySelector).html('<span style="color: green;">&#10004;</span>&nbsp;&nbsp;<span style="background-color: #308446; padding: 2px 5px; border: 1px solid green; border-radius: 30px; -webkit-backface-visibility: hidden; backface-visibility: hidden;">' + $(mySelector).text() + '</span>');
-			       $(mySelector).html('<span style="padding: 5px 10px; border-radius: 5px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); background: linear-gradient(to right, #00ff00, #00e600); color: white;">' + $(mySelector).text() + '</span>');
+    $(mySelector).html('<span style="color: green;">&#10004;</span>&nbsp;&nbsp;<span style="background-color: #308446; padding: 2px 2px 2px 2px; border: 1px solid green; border-radius: 35px; -webkit-backface-visibility: hidden; backface-visibility: hidden;">' + $(mySelector).text() + '</span>');
 }
 		   } else {
 	               if ($(mySelector).text() == parserName[i]) $(mySelector).html('&#10008;&nbsp;&nbsp;' + $(mySelector).text()).css('color', 'ff0000');
