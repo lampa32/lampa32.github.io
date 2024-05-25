a9233a48e95fb1ac85995a1855a59da9aab876b2
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-25 14:35:11 +0300

    Update freetorr.js

--- freetorr.js
+++ freetorr.js
@@ -230,6 +230,9 @@ Lampa.SettingsApi.addParam({
 						  $('div[data-name="torrserver_url_two"]').hide()
 					          $('div[data-name="torrserver_use_link"]').hide()
 						  $('div[data-name="switch_server_button"]').hide()
+						    setTimeout(function(){
+	                                                $('#SWITCH_SERVER').hide();
+		                                    }, 10)
 					    }
 					 }, 0);
                                }
