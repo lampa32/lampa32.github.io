a76b640e0ed0afd3996c6d2693cc7a7cd72b40fa
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-05-25 08:24:07 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -131,7 +131,7 @@ Lampa.SettingsApi.addParam({
                                 }
 				if(Lampa.Storage.field('parser_use')&&Lampa.Storage.field('parser_torrent_type') == 'jackett') item.show()&$('.settings-param__name', item).css('color','ffffff')&$('div[data-name="jackett_urltwo"]').insertAfter('div[data-name="parser_torrent_type"]');
 				else item.hide();
-			}, 20);
+			}, 0);
         }
    });
 	Lampa.Settings.listener.follow('', function (e) {
