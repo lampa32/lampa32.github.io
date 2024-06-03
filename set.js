70644a3fcaeea85f2d38c01ad8df5ded920856d0
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-06-03 15:25:48 +0300

    Update set.js

--- set.js
+++ set.js
@@ -118,12 +118,18 @@
 
 	
 	function cub_off() {
+
+               // убираем везде кнопку "Подписаться" через стиль
+          var styleElement = document.createElement('style');
+          styleElement.innerHTML = '.button--subscribe { display: none; }';
+          document.body.appendChild(styleElement);
+		
 		// убираем рекламу перед включением плеера через смену региона (не языка)
-        $(document).ready(function() {
+          $(document).ready(function() {
             var date = new Date(),
                 time = date.getTime()
             localStorage.setItem("region", '{"code":"uk","time":' + time + '}')
-        })
+          })
 		// удаляем рекламу в разделе Сериалов
 		$('[data-action="tv"]').on('hover:enter hover:click hover:touch', function() {
 			var myTextBoxInterval = setInterval(function() {
@@ -170,7 +176,7 @@
                 // скрываем кнопку ПОДПИСАТЬСЯ в карточке
 		setTimeout(function() {
 		    //if ($('.view--online').length > 1) $('.view--online')[1].remove();//убираем дубликат кнопки 
-                    $('.button--subscribe').remove();
+                    //$('.button--subscribe').remove();
 		    $(".view--online", Lampa.Activity.active().activity.render()).empty().append('<svg viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32"><path d="m17 14.5 4.2-4.5L4.9 1.2c-.1-.1-.3-.1-.6-.2L17 14.5zM23 21l5.9-3.2c.7-.4 1.1-1 1.1-1.8s-.4-1.5-1.1-1.8L23 11l-4.7 5 4.7 5zM2.4 1.9c-.3.3-.4.7-.4 1.1v26c0 .4.1.8.4 1.2L15.6 16 2.4 1.9zM17 17.5 4.3 31c.2 0 .4-.1.6-.2L21.2 22 17 17.5z" fill="currentColor" fill="#ffffff" class="fill-000000"></path></svg>Онлайн');
 			Lampa.Controller.toggle('full_start');
                 }, 0);
