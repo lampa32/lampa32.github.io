41de2d652fa3b7f8364cf58c6236c991414a6a13
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-06-07 00:17:56 +0300

    Update freetorr.js

--- freetorr.js
+++ freetorr.js
@@ -92,10 +92,10 @@ function checkAlive() {
 	$('#app > div.head > div > div.head__actions').append(switch_serverBUTT);
 	$('#SWITCH_SERVER').insertAfter('div[class="head__action selector open--settings"]');
        
-	if(Lampa.Storage.get('switch_server_button') == 1 || Lampa.Storage.get('torrserv') == 0) hideBut();
-		/*setTimeout(function(){
+	if(/*Lampa.Storage.get('switch_server_button') == 1 || */Lampa.Storage.get('torrserv') == 0)
+		setTimeout(function(){
                    $('#SWITCH_SERVER').hide()
-                }, 500);*/
+                }, 500);
 	if(Lampa.Storage.get('switch_server_button') == 2) showBut_Torr();
 	if(Lampa.Storage.get('switch_server_button') == 3) $('#SWITCH_SERVER').show()
 	
@@ -243,8 +243,7 @@ Lampa.SettingsApi.addParam({
 					if (value == '0') {
                                                 Lampa.Storage.set('torrserver_use_link', 'one');                                               
                                                 Lampa.Storage.set('torrserver_url_two', ''); 
-						//if(Lampa.Storage.get('switch_server_button') !== 1) hideBut();
-						hideBut();
+						if(Lampa.Storage.get('switch_server_button') !== 1) hideBut();
                                                 Lampa.Settings.update();
                                                 return;
                                         }
@@ -285,8 +284,7 @@ Lampa.SettingsApi.addParam({
                                                   Lampa.Controller.toggle('settings_component')
 						  $('div[data-name="torrserver_url_two"]').hide()
 					          $('div[data-name="torrserver_use_link"]').hide()
-						  $('div[data-name="switch_server_button"]').hide()  
-					   hideBut();
+						  $('div[data-name="switch_server_button"]').hide()
 					    }
 					 }, 0);
                                }
