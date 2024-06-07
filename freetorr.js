c7df7f3ef145a267fd82f0ae9674423ad2d06d90
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-06-07 10:22:40 +0300

    Update freetorr.js

--- freetorr.js
+++ freetorr.js
@@ -92,13 +92,13 @@ function checkAlive() {
 	$('#app > div.head > div > div.head__actions').append(switch_serverBUTT);
 	$('#SWITCH_SERVER').insertAfter('div[class="head__action selector open--settings"]');
        
-	if(Lampa.Storage.get('switch_server_button') == 1 || localStorage.getItem('torrserv') == 0)
+	if(Lampa.Storage.get('switch_server_button') == 1 || Lampa.Storage.get('torrserv') == 0)
 		setTimeout(function(){
                    $('#SWITCH_SERVER').hide()
                 }, 500);
 	if(Lampa.Storage.get('switch_server_button') == 2) showBut_Torr();
 	if(Lampa.Storage.get('switch_server_button') == 3) $('#SWITCH_SERVER').show()
-	
+	if(Lampa.Storage.get('torrserv') == 0) hideBut();
 	
 	$('#SWITCH_SERVER').on('hover:enter hover:click hover:touch', function() {
 		//start_free(); // если делать через функцию, будет тайм-аут
