add642a69a3de74460c416a1610bb4cf9c0c617d
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-08 14:18:15 +0300

    Update qual.js

--- qual.js
+++ qual.js
@@ -1,9 +1,9 @@
 (function () {
     'use strict';
 
-    function card() {
-      var apiKey = '?language=en-US&api_key=4ef0d7355d9ffb5151e987764708ce96';
-      var baseUrl = 'https://api.themoviedb.org/3/'; //http://tmdb.cub.red/3/';
+    /*function card() {
+      var apiKey = '4ef0d7355d9ffb5151e987764708ce96';
+      var baseUrl = 'http://tmdb.cub.red/3/';
       function fetchMovieDetails(movieId, method, callback) {
         var apiUrl = "".concat(baseUrl).concat(method, "/").concat(movieId, "?api_key=").concat(apiKey);
         $.getJSON(apiUrl, function (data, textStatus, xhr) {
@@ -16,45 +16,7 @@
           callback(new Error("Fetch error: ".concat(textStatus, ", ").concat(errorThrown)));
         });
       }
-      Lampa.Listener.follow("full", function (e) {
-        if (e.type === "complite" && Lampa.Storage.field('source') !== 'cub') {
-          $(document).ready(function () {
-            fetchMovieDetails(e.data.movie.id, e.object.method, function (err, data) {
-              if (err) {
-                console.error(err.message);
-                return;
-              }
-              var release_quality = data.release_quality;
-              if (release_quality) {
-                var newDivider = $("<span class='full-start-new__split'>").html("\u25CF");
-                //const newSpan = $("<span class='full-start__pg'>").html(`${Lampa.Lang.translate('player_quality')}: ${release_quality.toUpperCase()}`);
-                var newSpan = $("<span class='full-start__pg'>").html("".concat(Lampa.Lang.translate('player_quality'), ": ").concat(release_quality.toUpperCase())).css('border-color', function () {
-                  switch (release_quality.toLowerCase()) {
-                    case 'ts':
-                      return 'red';
-                    case 'webdl':
-                    case 'dvdrip':
-                      return 'yellow';
-                    case '4k':
-                    case 'bd':
-                      return 'green';
-                  }
-                }());
-                $(".full-start-new__details").append(newDivider, newSpan);
-                //Add label on poster
-                var quality = document.createElement('div');
-                quality.classList.add("card__quality");
-                quality.style.zIndex = "999";
-                quality.style.fontSize = "75%";
-                var quality_inner = document.createElement("div");
-                quality_inner.innerText = release_quality;
-                quality.appendChild(quality_inner);
-                if (Lampa.Platform.screen('mobile') == true) $(".full-start-new__poster").append(quality);
-              }
-            });
-          });
-        }
-      });
+      
       Lampa.Listener.follow("line", function (e) {
         if (e.type === "append" && Lampa.Storage.field("source") !== "cub") {
           e.items.forEach(function (movieCard) {
@@ -88,29 +50,95 @@
       card: card
     };
 
-   /* var manifest = {
-      type: "other",
-      version: "0.0.1",
-      author: '@lme_chat',
-      name: "LME Quality",
-      description: "Add Quality sticker on other source",
-      component: "lmeq"
-    };*/
+   
     function add() {
-     // Lampa.Manifest.plugins = manifest;
       UTILS.card();
     }
     
     
 
-    //function startPlugin() {
-     // window.plugin_lmeq_ready = true;
+   
       if (window.appready) add();else {
         Lampa.Listener.follow("app", function (e) {
           if (e.type === "ready") add();
         });
+      }*/
+  function card() {
+  var apiKey = '4ef0d7355d9ffb5151e987764708ce96';
+  var baseUrl = 'http://tmdb.cub.red/3/';
+
+  function fetchMovieDetails(movieId, mediaType) {
+    return new Promise(function (resolve, reject) {
+      var apiUrl = baseUrl + mediaType + "/" + movieId + "?api_key=" + apiKey;
+      $.getJSON(apiUrl, function (data, textStatus, xhr) {
+        if (xhr.status === 200) {
+          resolve(data);
+        } else {
+          reject(new Error("Error fetching movie details: " + data.status_message));
+        }
+      }).fail(function (jqXHR, textStatus, errorThrown) {
+        reject(new Error("Fetch error: " + textStatus + ", " + errorThrown));
+      });
+    });
+  }
+
+  Lampa.Listener.follow("line", function (e) {
+    if (e.type === "append" && Lampa.Storage.field("source") !== "cub") {
+      var movieIds = e.items
+        .filter(function (movieCard) {
+          return movieCard.data && (movieCard.data.id || movieCard.data.number_of_seasons);
+        })
+        .map(function (movieCard) {
+          return {
+            id: movieCard.data.id || 0,
+            mediaType: movieCard.data.media_type ? movieCard.data.media_type : movieCard.data.number_of_seasons ? "tv" : "movie"
+          };
+        });
+
+      if (movieIds.length > 0) {
+        Promise.all(movieIds.map(function (item) {
+          return fetchMovieDetails(item.id, item.mediaType);
+        }))
+        .then(function (results) {
+          e.items.forEach(function (movieCard, index) {
+            if (movieCard.data && (movieCard.data.id || movieCard.data.number_of_seasons)) {
+              var release_quality = results[index].release_quality;
+              if (release_quality) {
+                var quality = document.createElement("div");
+                quality.classList.add("card__quality");
+                var quality_inner = document.createElement("div");
+                quality_inner.innerText = release_quality;
+                quality.appendChild(quality_inner);
+                movieCard.card.querySelector(".card__view").appendChild(quality);
+              }
+            }
+          });
+        })
+        .catch(function (error) {
+          console.error("Error fetching movie details:", error);
+        });
       }
-   // }
-   // if (!window.plugin_lmeq_ready) startPlugin();
+    }
+  });
+}
+
+var UTILS = {
+  card: card
+};
+
+function add() {
+  UTILS.card();
+}
+
+if (window.appready) {
+  add();
+} else {
+  Lampa.Listener.follow("app", function (e) {
+    if (e.type === "ready") {
+      add();
+    }
+  });
+}
+
 
 })();
