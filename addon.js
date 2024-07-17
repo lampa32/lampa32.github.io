9edaf61f0c2881e03a7bf1c5d6b349b5e6a4870f
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-17 10:27:49 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -100,7 +100,7 @@ function showLoadingBar() {
   loadingBar.style.zIndex = '9999';
   loadingBar.style.display = 'none';
   loadingBar.style.width = '30em';//'300px';
-  loadingBar.style.height = '2em'; //'25px';
+  loadingBar.style.height = '2.5em'; //'25px';
   loadingBar.style.backgroundColor = '#595959';
   loadingBar.style.borderRadius = '4em';//'15px';
 
