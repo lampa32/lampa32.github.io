9741c7f9e78b0b4183960deb7a76609f49a16e8d
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-23 14:10:27 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -1,107 +1,93 @@
 (function () {
     'use strict';
-    Lampa.Platform.tv();
+  function main(){
 
-function start() {
-
-var stay = 0
-var server_protocol = location.protocol === "https:" ? 'https://' : 'http://'
-	
-function showServerInput() {
+    //var stay = 0
+    var server_protocol = location.protocol === "https:" ? 'https://' : 'http://'
+   
+    function showServerInput() {
       Lampa.Input.edit({
           title: "Укажите Сервер",
           value: '',
           free: true       
       }, function (value) {
-	 if (value !== '') {
-	window.location.href = server_protocol + value;
-	}
-	else {showMeExitMenu();}
+          if (value !== '') {
+          window.location.href = server_protocol + value;
+           }
+          else {
+            showMeExitMenu();
+          }
       })
-}
-
-function closeApp() {
-   Lampa.Activity.out();
-      if (Lampa.Platform.is('apple_tv')) window.location.assign('exit://exit');
-      if (Lampa.Platform.is("tizen")) tizen.application.getCurrentApplication().exit();
-      if (Lampa.Platform.is("webos")) window.close();
-      if (Lampa.Platform.is("android")) Lampa.Android.exit();
-      if (Lampa.Platform.is("orsay")) Lampa.Orsay.exit();
-      if (Lampa.Platform.is("nw")) nw.Window.get().close();
-      if (Lampa.Platform.is('netcast')) window.NetCastBack();
-      if (Lampa.Platform.is('browser')) window.close();
-}
-
-function showMeExitMenu() {
-  var enabled = Lampa.Controller.enabled().name;
-    var menu = [];
-	
-    menu.push({
-  title:  'Выход'
-    });
-
-    menu.push({
-  title:  'Перезагрузить'
-    });
-	
-  var separator = { type: 'separator' };
-    menu.push(separator);
-   
-	menu.push({
-  title:  'YouTube'
-    });
-
-    menu.push({
-  title:  'Сменить адрес'
-    });
-    Lampa.Select.show({
-    title: 'Выход',
-    items: menu,
-    onBack: function onBack() { 
-	    stay = 0;
-	    //Lampa.Controller.toggle(enabled);
-    },
-    onSelect: function onSelect(a) {
-      stay = 0;
-      if (a.title == 'Выход') closeApp();
-      if (a.title == 'Перезагрузить') location.reload();
-      if (a.title == 'YouTube') window.location.href = 'https://youtube.com/tv';
-      if (a.title == 'Сменить адрес') showServerInput();    
-      //Lampa.Controller.toggle(enabled);
     }
-    })
-}
-  
-  Lampa.Controller.listener.follow('toggle', function(e) {
-    if(e.name == 'select' && stay !== 1 && document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__head > div").innerText == Lampa.Lang.translate('title_out')) {
-      setTimeout(function() {
-        stay = 1
-        showMeExitMenu()
-      },10);
-    };
-  })
 
-	
-/*     Вместо пункта отмена, добавляем пункт
-        Перезагрузить при долгом тапе назад
-	
-Lampa.Controller.listener.follow('toggle', function(e) {
-    if(e.name == 'select') {
-      setTimeout(function() {
-        if (document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__head > div").innerText == Lampa.Lang.translate('title_out')) {
-          document.querySelector("body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__body.layer--wheight > div > div > div > div:nth-child(2) > div").text('Перезагрузить')
-          $('.selectbox-item')[1].on('hover:enter hover:click hover:touch', function() {
-            location.reload();
-          });
+    function closeApp() {
+        if (Lampa.Platform.is('apple_tv')) window.location.assign('exit://exit');
+        if (Lampa.Platform.is("tizen")) tizen.application.getCurrentApplication().exit();
+        if (Lampa.Platform.is("webos")) window.close();
+        if (Lampa.Platform.is("android")) Lampa.Android.exit();
+        if (Lampa.Platform.is("orsay")) Lampa.Orsay.exit();
+        if (Lampa.Platform.is("nw")) nw.Window.get().close();
+        if (Lampa.Platform.is('netcast')) window.NetCastBack();
+        if (Lampa.Platform.is('browser')) window.close();
+    }
+
+    function showMeExitMenu() {
+      var enabled = Lampa.Controller.enabled().name;
+      var menu = [];
+     
+      menu.push({
+        title: 'Выход'
+      });
+
+      menu.push({
+        title: 'Перезагрузить'
+      });
+     /*
+      menu.push({
+        type: 'separator' 
+      });
+    */   
+       menu.push({
+        title: 'YouTube'
+      });
+
+      menu.push({
+        title: 'Сменить адрес'
+      });
+      
+      Lampa.Select.show({
+        title: 'Выход ', // пробел обязателен, чтобы отделить событие от оригинального меню
+        items: menu,
+        onBack: function onBack() { 
+         //stay = 0;
+         Lampa.Controller.toggle('content');
+        },
+        onSelect: function onSelect(a) {
+          //stay = 0;
+          if (a.title == 'Выход') closeApp();
+          if (a.title == 'Перезагрузить') location.reload();
+          if (a.title == 'YouTube') window.location.href = 'https://youtube.com/tv';
+          if (a.title == 'Сменить адрес') showServerInput();    
+          //Lampa.Controller.toggle(enabled);
         }
-      }, 100);
+      })
     }
-  });  */
-	
-	
-} 
-  
-if(window.appready) start();
-else Lampa.Listener.follow('app', function(e) {	if(e.type == 'ready') start(); });
+    
+    Lampa.Controller.listener.follow('toggle', function(e) {
+      if (e.name == 'select' && $('.selectbox__title').text() == Lampa.Lang.translate('title_out')) {
+        Lampa.Select.hide();
+        setTimeout(function() {
+          showMeExitMenu()
+        },100);
+      };
+    })
 
-})();
+  } // завершаем main
+
+    if (window.appready) main();
+    else {
+        Lampa.Listener.follow('app', function(e) {
+          if (e.type == 'ready') main();
+        });
+    }  
+})()
