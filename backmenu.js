41274a857e82696d2825f8ff988d73b4d0c097f3
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-24 10:57:42 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -4,8 +4,8 @@
     
 function main(){
 
-     var trigger_on = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z" fill="#4CAF50"/></svg>'
-     var trigger_off = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#F44336"/></svg>'
+     var trigger_on = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z" fill="#4CAF50"/></svg>';
+     var trigger_off = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#F44336"/></svg>';
      
     Lampa.Storage.listener.follow('change', function (e) {});
                 Lampa.Settings.listener.follow('open', function (e) {
@@ -58,8 +58,8 @@ function main(){
                                                icon: {
                                                      on: trigger_on,
                                                      off: trigger_off
-                                               },
-                               },         
+                                               }
+                                       },         
         });
     
         Lampa.SettingsApi.addParam({
