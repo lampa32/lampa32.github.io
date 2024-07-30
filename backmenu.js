a488674a7d8eedd46b9fa027ff022a3947d1fb1a
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-30 19:21:30 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -245,7 +245,7 @@ function back_menu(){
     } 
 
     function modal_speedtest() {
-					var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;background-color: #ffffff;overflow:hidden !important;" src="https://speedtest.tatar.ru/?run=5"></iframe></div></div></div>');
+					var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;background-color: #ffffff;overflow:hidden !important;" src="http://st.agtel.net"></iframe></div></div></div>');
 		  			Lampa.Modal.open({
 		  				title: '',
 		  				html: modal,
@@ -253,7 +253,7 @@ function back_menu(){
 		  				mask: true, 
 		  				onBack: function onBack() {
 		  					Lampa.Modal.close();
-							Lampa.Controller.toggle('settings_component');
+							Lampa.Controller.toggle('content');
 		  				},
 		  				onSelect: function () {}
   			                 });
