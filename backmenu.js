02616de40a13ca1af973b4ed2f9018205a59dffc
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-07-31 10:26:42 +0300

    Update backmenu.js

--- backmenu.js
+++ backmenu.js
@@ -245,7 +245,7 @@ function back_menu(){
     } 
 
     function modal_speedtest() {
-	 var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;background-color: #ffffff;overflow:hidden !important;" src="https://zamerka.com"></iframe></div></div></div>');
+	 var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;background-color: #ffffff;overflow:hidden !important;" src="http://zamerka.com"></iframe></div></div></div>');
 	 Lampa.Modal.open({
 		title: '',
 		html: modal,
