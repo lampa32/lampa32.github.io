1bd92aa030d4d07ec57178adef9ea5f4ca57663b
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-08-01 15:56:02 +0300

    Update account.js

--- account.js
+++ account.js
@@ -12,7 +12,7 @@
 
      function checkToken(token, callback) {
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://localhost:3000/checkToken', true);
+  xhr.open('POST', 'http://212.113.103.137:3000/checkToken', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
