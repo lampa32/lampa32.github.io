7aeafc9bb2e79e82e144fa1f74098b5f4637fe36
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-08-01 18:12:52 +0300

    Update account.js

--- account.js
+++ account.js
@@ -1,3 +1,6 @@
+(function () {
+    'use strict';
+
 Lampa.Settings.listener.follow('open', function (e) {
             if (e.name == 'main') {
                 Lampa.SettingsApi.addComponent({
@@ -8,17 +11,40 @@ Lampa.Settings.listener.follow('open', function (e) {
       });
 
       Lampa.SettingsApi.addParam({
-                               component: 'add_acc',
-                               param: {
-                                       name: 'auth',
-                                       type: 'input',
-                                       values: '',
-                                       placeholder: 'Введите token',
-                                       default: ''
-                               },
-                               field: {
-                                   name: 'Выполнить вход',
-                                   description: ''
-                               },
+  component: 'adLampa.SettingsApi.addParam({
+  component: 'add_acc',
+  param: {
+    name: 'auth',
+    type: 'input',
+    values: '',
+    placeholder: 'Введите token',
+    default: ''
+  },
+  field: {
+    name: 'Выполнить вход',
+    description: ''
+  },
+  onSave: async (value) => {
+    try {
+      const response = await fetch('http://212.113.103.137:3000/checkToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: value })
+      });
+      const { userId } = await response.json();
+      if (userId) {
+        // Токен действителен, сохраняем его
+        localStorage.setItem('token', value);
+      } else {
+        // Токен недействителен, очищаем его
+        localStorage.removeItem('token');
+      }
+    } catch (error) {
+      console.error('Ошибка проверки токена:', error);
+    }
+  }
+});
 
-     });
+})();
