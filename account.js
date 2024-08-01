630fcb4f724d3a7f220082258587682b4dc98334
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-08-01 15:28:55 +0300

    Update account.js

--- account.js
+++ account.js
@@ -10,28 +10,58 @@
 			      }
       });
 
-      Lampa.SettingsApi.addParam({
-                               component: 'add_acc',
-                               param: {
-                                       name: 'auth',
-                                       type: 'input',
-                                       values: '',
-					                             placeholder: 'Введите token',
-					                             default: ''
-				                       },
-                               field: {
-					                         name: 'Выполнить вход',
-					                         description: ''
-				                       },
-				                       /*onChange: function (value) {
-				   
-				                           if (value == '') {
-					                              $('#REDIRECT').remove()
-				                           }
-				                           if (!value == '') {
-					                             startMe();
-				                           }
-				                      } */        
-     });
+     // Используем jQuery вместо Axios
+var $ = require('jquery');
+
+Lampa.SettingsApi.addParam({
+  component: 'add_acc',
+  param: {
+    name: 'auth',
+    type: 'input',
+    values: '',
+    placeholder: 'Введите token',
+    default: '',
+    onchange: function(value) {
+      // Используем callback вместо async/await
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/checkToken',
+        data: JSON.stringify({ token: value }),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function(response) {
+          var userId = response.userId;
+          if (userId) {
+            // Токен верный, можно сохранить userId в приложении
+            saveUserId(userId);
+            Lampa.Tools.notify('Успешная авторизация!');
+          } else {
+            Lampa.Tools.notify('Неверный токен');
+          }
+        },
+        error: function(error) {
+          console.error('Ошибка при проверке токена:', error);
+          Lampa.Tools.notify('Ошибка при проверке токена');
+        }
+      });
+    }
+  },
+  field: {
+    name: 'Выполнить вход',
+    description: ''
+  }
+});
+
+function saveUserId(userId) {
+  // Используем localStorage для сохранения userId
+  localStorage.setItem('userId', userId);
+}
+
+function getUserId() {
+  // Используем localStorage для получения userId
+  return localStorage.getItem('userId');
+}
+
+
 
 })();
