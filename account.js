832f88d24bf77d23aa860926ce1da8f40955d9f7
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-08-01 22:29:24 +0300

    Update account.js

--- account.js
+++ account.js
@@ -10,7 +10,7 @@ Lampa.Settings.listener.follow('open', function (e) {
             }
       });
 
-      Lampa.SettingsApi.addParam({
+      /*Lampa.SettingsApi.addParam({
   component: 'add_acc',
   param: {
     name: 'auth',
@@ -45,6 +45,41 @@ Lampa.Settings.listener.follow('open', function (e) {
     } catch (error) {
       console.log('Ошибка проверки токена:', error);
     }
+  }
+});*/
+
+    Lampa.SettingsApi.addParam({
+  component: 'add_acc',
+  param: {
+    name: 'auth',
+    type: 'input',
+    values: '',
+    placeholder: 'Введите token',
+    default: ''
+  },
+  field: {
+    name: 'Выполнить вход',
+    description: ''
+  },
+  onChange: (value) => {
+    console.log('Введенный токен:', value);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://212.113.103.137:3000/checkToken', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log('Ответ сервера:', response);
+        if (response.userId) {
+          console.log('Токен действителен');
+          localStorage.setItem('token', value);
+        } else {
+          console.log('Токен недействителен');
+          localStorage.removeItem('token');
+        }
+      }
+    };
+    xhr.send(JSON.stringify({ token: value }));
   }
 });
 
