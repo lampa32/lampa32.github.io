d4d0d1701e16f84fa0455f5e9b38c729db9a57cb
lampa32 <140617338+lampa32@users.noreply.github.com>
2024-08-02 09:06:39 +0300

    Update account.js

--- account.js
+++ account.js
@@ -10,43 +10,7 @@ Lampa.Settings.listener.follow('open', function (e) {
             }
       });
 
-      /*Lampa.SettingsApi.addParam({
-  component: 'add_acc',
-  param: {
-    name: 'auth',
-    type: 'input',
-    values: '',
-    placeholder: 'Введите token',
-    default: ''
-  },
-  field: {
-    name: 'Выполнить вход',
-    description: ''
-  },
-  onChange: async (value) => {
-    try {
-      const response = await fetch('http://212.113.103.137:3000/checkToken', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ token: value })
-      });
-      const { userId } = await response.json();
-      if (userId) {
-          console.log('Токен действителен');
-        // Токен действителен, сохраняем его
-        localStorage.setItem('token', value);
-      } else {
-          console.log('Токен недействителен');
-        // Токен недействителен, очищаем его
-        localStorage.removeItem('token');
-      }
-    } catch (error) {
-      console.log('Ошибка проверки токена:', error);
-    }
-  }
-});*/
+     
 
     Lampa.SettingsApi.addParam({
   component: 'add_acc',
